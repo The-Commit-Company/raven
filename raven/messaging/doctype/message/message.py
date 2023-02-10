13927296847821d9e2d1765c847d52b28216e5d2
Aditya Patil <patiladitya781@gmail.com>
2023-02-10 19:40:51 +0530

    created message doctype

---
+++ raven/messaging/doctype/message/message.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Janhvi Patil and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Message(Document):
+	pass
