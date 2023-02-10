dd17fc407f6b251a4c6737c8f5ae4e044dd359c6
Janhvi Patil <janhvipatil716@gmail.com>
2023-02-10 11:17:05 +0530

    installed UI library

--- raven-app/src/main.tsx
+++ raven-app/src/main.tsx
@@ -1,10 +1,22 @@
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './index.css'
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter basename={import.meta.env.VITE_BASE_NAME ?? ''}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
