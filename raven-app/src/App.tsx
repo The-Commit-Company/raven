5bf987eb46f90f990dde2e36718cf21e592cf863
Aditya Patil <patiladitya781@gmail.com>
2023-02-11 22:27:10 +0530

    updated routes

--- raven-app/src/App.tsx
+++ raven-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { FrappeProvider } from 'frappe-react-sdk'
 import { Route, Routes } from 'react-router-dom'
 import { TestPage } from './pages/TestPage'
+import { ProtectedRoute } from './utils/auth/ProtectedRoute'
 import { UserProvider } from './utils/auth/UserProvider'
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
     <FrappeProvider url={import.meta.env.VITE_FRAPPE_PATH ?? ''}>
       <UserProvider>
         <Routes>
-          <Route path="/" element={<TestPage />} />
+          <Route path="/" element={<ProtectedRoute />}>
+            <Route index element={<TestPage />} />
+          </Route>
         </Routes>
       </UserProvider>
     </ FrappeProvider>
