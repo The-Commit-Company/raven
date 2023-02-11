306ad4f17ba7c74ea039409ae971abcbe1615bb8
Aditya Patil <patiladitya781@gmail.com>
2023-02-11 18:48:53 +0530

    - added chat history - realtime chat update

--- raven-app/src/components/feature/chat/ChatInterface.tsx
+++ raven-app/src/components/feature/chat/ChatInterface.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@chakra-ui/react"
 import { useFrappeGetDocList } from "frappe-react-sdk"
 import { useContext, useMemo } from "react"
+import { useFrappeEventListener } from "../../../hooks/useFrappeEventListener"
 import { ChannelContext } from "../../../utils/channel/ChannelContext"
 import { ChatHistory } from "./ChatHistory"
 import { ChatInput } from "./ChatInput"
@@ -9,19 +10,32 @@ interface Message {
     text: string
     user_id: string
     creation: string
+    name: string
 }
 
 export const ChatInterface = () => {
     const { channelID } = useContext(ChannelContext)
 
-    const { data } = useFrappeGetDocList<Message>('Message', {
-        fields: ["text", "creation"],
-        filters: [["channel_id", "=", channelID]]
+    const { data, error, mutate } = useFrappeGetDocList<Message>('Message', {
+        fields: ["text", "creation", "name", "user_id"],
+        filters: [["channel_id", "=", channelID]],
+        orderBy: {
+            field: "creation",
+            order: 'asc'
+        }
+    })
+
+    useFrappeEventListener('message_received', (data) => {
+        if (data.channel_id === channelID) {
+            mutate()
+            console.log('message received')
+        }
     })
 
     const messages: Message[] = useMemo(() => {
         if (data) {
             return data.map((message) => ({
+                name: message.name,
                 text: message.text,
                 user_id: message.user_id,
                 creation: message.creation
