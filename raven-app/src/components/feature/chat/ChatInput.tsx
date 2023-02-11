270f643956359b5d862388165fcd5aebbc417300
Aditya Patil <patiladitya781@gmail.com>
2023-02-11 15:03:09 +0530

    basic messaging functionality created

--- raven-app/src/components/feature/chat/ChatInput.tsx
+++ raven-app/src/components/feature/chat/ChatInput.tsx
@@ -1,19 +1,19 @@
 import { chakra, Flex, IconButton, Input } from "@chakra-ui/react"
 import { useFrappePostCall } from "frappe-react-sdk"
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { FormProvider, useForm } from "react-hook-form"
 import { RiSendPlaneFill } from "react-icons/ri"
+import { UserContext } from "../../../utils/auth/UserProvider"
 
 interface ChatInputProps {
     addMessage: (text: string, user: string) => Promise<void>
 }
 
 export const ChatInput = ({ addMessage }: ChatInputProps) => {
-
-    const user_id = "patiladitya781@gmail.com"
+    const { currentUser } = useContext(UserContext)
     const channel_id = "862bf4d1ce"
 
-    // const { call, loading, error, reset } = useFrappePostCall('raven.messaging.doctype.message.message.send_message')
+    const { call, loading, error, reset } = useFrappePostCall('raven.messaging.doctype.message.message.send_message')
 
     const [text, setText] = useState("")
     const methods = useForm()
@@ -27,18 +27,12 @@ export const ChatInput = ({ addMessage }: ChatInputProps) => {
     }
 
     const onSubmit = () => {
-        fetch('http://localhost:8080/api/method/raven.messaging.doctype.message.message.send_message', {
-            method: 'POST',
-            headers: {
-                'Authorization': 'token 70fc5baf0dbedeb:153d36b83606c05'
-            },
-            body: JSON.stringify({
-                'channel_id': channel_id,
-                'user_id': user_id,
-                'text': text
-            })
+        call({
+            user_id: currentUser,
+            channel_id: channel_id,
+            text: text
         }).then(() =>
-            addMessage(text, user_id)
+            addMessage(text, currentUser)
         ).then(() => {
             setText("")
         })
