862b600a4e225020c13f66dd0e50cc98142caf68
Janhvi Patil <janhvipatil716@gmail.com>
2023-02-12 17:39:09 +0530

    fix: renamed channel member in api

--- raven/raven_channel_management/doctype/raven_channel_member/raven_channel_member.py
+++ raven/raven_channel_management/doctype/raven_channel_member/raven_channel_member.py
@@ -1,8 +1,24 @@
 # Copyright (c) 2023, Janhvi Patil and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from pypika import JoinType
 
 class RavenChannelMember(Document):
 	pass
+
+
+@frappe.whitelist()
+def get_all_channel_members(channel_id):
+    # fetch all channel members
+    # get member details from user table, such as name, full_name, user_image, first_name
+    channel_member = frappe.qb.DocType('Raven Channel Member')
+    user = frappe.qb.DocType('User')
+    query = (frappe.qb.from_(channel_member)
+             .join(user, JoinType.left)
+             .on(channel_member.user_id == user.name)
+             .select(user.name, user.full_name, user.user_image, user.first_name)
+             .where(channel_member.channel_id == channel_id))
+
+    return query.run(as_dict=True)
