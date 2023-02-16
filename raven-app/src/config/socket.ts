0c0e5f4816e8a8e99f95dc3e04af5d6d0a50f267
Aditya Patil <patiladitya781@gmail.com>
2023-02-16 13:54:18 +0530

    feat: mentions added

--- raven-app/src/config/socket.ts
+++ raven-app/src/config/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-let socket_port = import.meta.env.VITE_SOCKET_PORT ?? "9004"
+let socket_port = import.meta.env.VITE_SOCKET_PORT ?? "9002"
 let host = window.location.hostname;
 let port = window.location.port ? `:${socket_port}` : '';
 let protocol = port ? 'http' : 'https';
