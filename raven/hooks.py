81de1c4ecc428c43489720e90da05e1b2fa8ee9d
Aditya Patil <patiladitya781@gmail.com>
2023-02-17 17:11:29 +0530

    feat: added DMs

--- raven/hooks.py
+++ raven/hooks.py
@@ -42,7 +42,7 @@ app_license = "MIT"
 
 # website user home page (by Role)
 # role_home_page = {
-#	"Role": "home_page"
+# "Role": "home_page"
 # }
 
 # Generators
@@ -56,8 +56,8 @@ app_license = "MIT"
 
 # add methods and filters to jinja environment
 # jinja = {
-#	"methods": "raven.utils.jinja_methods",
-#	"filters": "raven.utils.jinja_filters"
+# "methods": "raven.utils.jinja_methods",
+# "filters": "raven.utils.jinja_filters"
 # }
 
 # Installation
@@ -65,6 +65,7 @@ app_license = "MIT"
 
 # before_install = "raven.install.before_install"
 # after_install = "raven.install.after_install"
+after_sync = "raven.raven_channel_management.doctype.raven_channel.raven_channel.create_initial_direct_message_channels_for_all"
 
 # Uninstallation
 # ------------
@@ -83,11 +84,11 @@ app_license = "MIT"
 # Permissions evaluated in scripted ways
 
 # permission_query_conditions = {
-#	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# "Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
 # }
 #
 # has_permission = {
-#	"Event": "frappe.desk.doctype.event.event.has_permission",
+# "Event": "frappe.desk.doctype.event.event.has_permission",
 # }
 
 # DocType Class
@@ -95,40 +96,38 @@ app_license = "MIT"
 # Override standard doctype classes
 
 # override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
+# "ToDo": "custom_app.overrides.CustomToDo"
 # }
 
 # Document Events
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-#	"*": {
-#		"on_update": "method",
-#		"on_cancel": "method",
-#		"on_trash": "method"
-#	}
-# }
+doc_events = {
+    "User": {
+        "after_insert": "raven.raven_channel_management.doctype.raven_channel.raven_channel.create_initial_direct_message_channels_for_user"
+    }
+}
 
 # Scheduled Tasks
 # ---------------
 
 # scheduler_events = {
-#	"all": [
-#		"raven.tasks.all"
-#	],
-#	"daily": [
-#		"raven.tasks.daily"
-#	],
-#	"hourly": [
-#		"raven.tasks.hourly"
-#	],
-#	"weekly": [
-#		"raven.tasks.weekly"
-#	],
-#	"monthly": [
-#		"raven.tasks.monthly"
-#	],
+# "all": [
+# "raven.tasks.all"
+# ],
+# "daily": [
+# "raven.tasks.daily"
+# ],
+# "hourly": [
+# "raven.tasks.hourly"
+# ],
+# "weekly": [
+# "raven.tasks.weekly"
+# ],
+# "monthly": [
+# "raven.tasks.monthly"
+# ],
 # }
 
 # Testing
@@ -140,14 +139,14 @@ app_license = "MIT"
 # ------------------------------
 #
 # override_whitelisted_methods = {
-#	"frappe.desk.doctype.event.event.get_events": "raven.event.get_events"
+# "frappe.desk.doctype.event.event.get_events": "raven.event.get_events"
 # }
 #
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
 # along with any modifications made in other Frappe apps
 # override_doctype_dashboards = {
-#	"Task": "raven.task.get_dashboard_data"
+# "Task": "raven.task.get_dashboard_data"
 # }
 
 # exempt linked doctypes from being automatically cancelled
@@ -164,31 +163,32 @@ app_license = "MIT"
 # --------------------
 
 # user_data_fields = [
-#	{
-#		"doctype": "{doctype_1}",
-#		"filter_by": "{filter_by}",
-#		"redact_fields": ["{field_1}", "{field_2}"],
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_2}",
-#		"filter_by": "{filter_by}",
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_3}",
-#		"strict": False,
-#	},
-#	{
-#		"doctype": "{doctype_4}"
-#	}
+# {
+# "doctype": "{doctype_1}",
+# "filter_by": "{filter_by}",
+# "redact_fields": ["{field_1}", "{field_2}"],
+# "partial": 1,
+# },
+# {
+# "doctype": "{doctype_2}",
+# "filter_by": "{filter_by}",
+# "partial": 1,
+# },
+# {
+# "doctype": "{doctype_3}",
+# "strict": False,
+# },
+# {
+# "doctype": "{doctype_4}"
+# }
 # ]
 
 # Authentication and authorization
 # --------------------------------
 
 # auth_hooks = [
-#	"raven.auth.validate"
+# "raven.auth.validate"
 # ]
 
-website_route_rules = [{'from_route': '/raven-app/<path:app_path>', 'to_route': 'raven-app'},]
+website_route_rules = [
+    {'from_route': '/raven-app/<path:app_path>', 'to_route': 'raven-app'},]
