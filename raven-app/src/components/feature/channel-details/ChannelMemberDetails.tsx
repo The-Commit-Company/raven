dc1a5298e9ee90deb3a6301705152a68841590ef
Janhvi Patil <janhvipatil716@gmail.com>
2023-02-20 16:00:04 +0530

    added remove member function

--- raven-app/src/components/feature/channel-details/ChannelMemberDetails.tsx
+++ raven-app/src/components/feature/channel-details/ChannelMemberDetails.tsx
@@ -1,18 +1,15 @@
 import { SearchIcon } from "@chakra-ui/icons"
-import { Text, Avatar, HStack, Icon, IconButton, Input, InputGroup, InputLeftElement, List, ListItem, Stack, useColorMode, useDisclosure } from "@chakra-ui/react"
+import { Text, Avatar, HStack, Icon, IconButton, Input, InputGroup, InputLeftElement, List, ListItem, Stack, useColorMode, useDisclosure, Button } from "@chakra-ui/react"
 import { useState } from "react"
 import { GoPrimitiveDot } from "react-icons/go"
 import { RiUserAddLine } from "react-icons/ri"
 import { useDebounce } from "../../../hooks/useDebounce"
 import { AddChannelMemberModal } from "../channels/AddChannelMemberModal"
+import { RemoveChannelMemberModal } from "./EditChannelDetails/RemoveChannelMemberModal"
+import { User } from "../../../types/User/User"
 
 interface MemberDetailsProps {
-    members: {
-        name: string,
-        full_name: string,
-        user_image: string,
-        first_name: string
-    }[]
+    members: User[]
 }
 
 export const ChannelMemberDetails = ({ members }: MemberDetailsProps) => {
@@ -31,7 +28,14 @@ export const ChannelMemberDetails = ({ members }: MemberDetailsProps) => {
         cursor: 'pointer'
     }
 
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen: isAddMembersModalOpen, onOpen: onAddMembersModalOpen, onClose: onAddMembersModalClose } = useDisclosure()
+    const { isOpen: isRemoveMembersModalOpen, onOpen: onRemoveMembersModalOpen, onClose: onRemoveMembersModalClose } = useDisclosure()
+    const [selectedMember, setSelectedMember] = useState('')
+
+    const onMemberSelect = (member: User) => {
+        setSelectedMember(member.name)
+        onRemoveMembersModalOpen()
+    }
 
     return (
         <Stack spacing={4}>
@@ -49,7 +53,7 @@ export const ChannelMemberDetails = ({ members }: MemberDetailsProps) => {
 
             <List spacing={2}>
 
-                <ListItem _hover={{ ...LISTHOVERSTYLE }} rounded='md' onClick={onOpen}>
+                <ListItem _hover={{ ...LISTHOVERSTYLE }} rounded='md' onClick={onAddMembersModalOpen}>
                     <HStack p='2' spacing={3}>
                         <IconButton
                             size='sm'
@@ -64,20 +68,24 @@ export const ChannelMemberDetails = ({ members }: MemberDetailsProps) => {
                 {members.map(member => {
                     return (
                         <ListItem key={member.name} _hover={{ ...LISTHOVERSTYLE }} rounded='md'>
-                            <HStack p='2' spacing={3}>
-                                <Avatar size='sm' src={member.user_image} name={member.full_name} borderRadius='md' />
-                                <HStack spacing={1}>
-                                    <Text fontWeight='semibold'>{member.first_name}</Text>
-                                    <Icon as={GoPrimitiveDot} color='green.600' />
-                                    <Text fontWeight='light'>{member.full_name}</Text>
+                            <HStack justifyContent='space-between' pr='3'>
+                                <HStack p='2' spacing={3}>
+                                    <Avatar size='sm' src={member.user_image} name={member.full_name} borderRadius='md' />
+                                    <HStack spacing={1}>
+                                        <Text fontWeight='semibold'>{member.first_name}</Text>
+                                        <Icon as={GoPrimitiveDot} color='green.600' />
+                                        <Text fontWeight='light'>{member.full_name}</Text>
+                                    </HStack>
                                 </HStack>
+                                <Button colorScheme='blue' variant='link' size='xs' onClick={() => onMemberSelect(member)}>Remove</Button>
                             </HStack>
                         </ListItem>
                     )
                 })}
 
             </List>
-            <AddChannelMemberModal isOpen={isOpen} onClose={onClose} />
+            <AddChannelMemberModal isOpen={isAddMembersModalOpen} onClose={onAddMembersModalClose} />
+            <RemoveChannelMemberModal isOpen={isRemoveMembersModalOpen} onClose={onRemoveMembersModalClose} user_id={selectedMember} />
         </Stack>
     )
 }
