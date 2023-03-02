9f8b2496676861c936bd2a4d179893bd0fd5ef0f
Aditya Patil <patiladitya781@gmail.com>
2023-03-02 22:41:05 +0530

    fixed: Administrator won't showup as any channel's member

--- raven/raven_channel_management/doctype/raven_channel_member/raven_channel_member.py
+++ raven/raven_channel_management/doctype/raven_channel_member/raven_channel_member.py
@@ -9,12 +9,15 @@ from pypika import JoinType
 class RavenChannelMember(Document):
     def validate(self):
         if not self.check_if_user_is_member():
-            frappe.throw("You don't have permission to add/modify members in this channel", frappe.PermissionError)
-    
+            frappe.throw(
+                "You don't have permission to add/modify members in this channel", frappe.PermissionError)
+
     def before_insert(self):
-        #1. A user cannot be a member of a channel more than once
+        # 1. A user cannot be a member of a channel more than once
         if frappe.db.exists("Raven Channel Member", {"channel_id": self.channel_id, "user_id": self.user_id}):
-            frappe.throw("You are already a member of this channel", frappe.DuplicateEntryError)
+            frappe.throw("You are already a member of this channel",
+                         frappe.DuplicateEntryError)
+
     def after_insert(self):
         frappe.publish_realtime('member_added', {
             'channel_id': self.channel_id}, after_commit=True)
@@ -24,21 +27,23 @@ class RavenChannelMember(Document):
         frappe.publish_realtime('member_removed', {
             'channel_id': self.channel_id}, after_commit=True)
         frappe.db.commit()
-    
+
     def on_trash(self):
         if not self.check_if_user_is_member():
-            frappe.throw("You don't have permission to remove members from this channel", frappe.PermissionError)
-    
+            frappe.throw(
+                "You don't have permission to remove members from this channel", frappe.PermissionError)
+
     def check_if_user_is_member(self):
         is_member = True
-        channel = frappe.db.get_value("Raven Channel", self.channel_id, ["type", "owner"], as_dict=True)
+        channel = frappe.db.get_value("Raven Channel", self.channel_id, [
+                                      "type", "owner"], as_dict=True)
         if channel.type == "Private":
-            #A user can only add members to a private channel if they are themselves member of the channel or if they are the owner of a new channel
+            # A user can only add members to a private channel if they are themselves member of the channel or if they are the owner of a new channel
             if channel.owner == frappe.session.user and frappe.db.count("Raven Channel Member", {"channel_id": self.channel_id}) == 0:
-                #User is the owner of a channel and there are no members in the channel
+                # User is the owner of a channel and there are no members in the channel
                 pass
             elif frappe.db.exists("Raven Channel Member", {"channel_id": self.channel_id, "user_id": frappe.session.user}):
-                #User is a member of the channel
+                # User is a member of the channel
                 pass
             else:
                 is_member = False
@@ -56,7 +61,8 @@ def get_channel_members_and_data(channel_id):
                     .join(user, JoinType.left)
                     .on(channel_member.user_id == user.name)
                     .select(user.name, user.full_name, user.user_image, user.first_name)
-                    .where(channel_member.channel_id == channel_id))
+                    .where(channel_member.channel_id == channel_id)
+                    .where(channel_member.user_id != "administrator"))
     data_query = (frappe.qb.from_(channel_data)
                   .select(channel_data.name,
                           channel_data.channel_name,
