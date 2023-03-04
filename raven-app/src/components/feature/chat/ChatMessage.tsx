30ac8bf67dc9bf38e93b2c1e2d0e4d2d2a1292ee
Janhvi Patil <janhvipatil716@gmail.com>
2023-03-04 11:48:27 +0530

    added edit/delete message button to be shown on message hover

--- raven-app/src/components/feature/chat/ChatMessage.tsx
+++ raven-app/src/components/feature/chat/ChatMessage.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Box, HStack, Stack, StackDivider, Text } from "@chakra-ui/react"
-import { useContext } from "react"
+import { Avatar, Box, ButtonGroup, HStack, IconButton, Stack, StackDivider, Text } from "@chakra-ui/react"
+import { useContext, useState } from "react"
+import { RiDeleteBinLine, RiEdit2Line } from "react-icons/ri";
 import { ChannelContext } from "../../../utils/channel/ChannelProvider"
 import { DateObjectToTimeString } from "../../../utils/operations";
 import { MarkdownRenderer } from "../markdown-viewer/MarkdownRenderer.tsx";
@@ -13,15 +14,28 @@ interface ChatMessageProps {
 export const ChatMessage = ({ text, user, timestamp }: ChatMessageProps) => {
 
     const { channelMembers } = useContext(ChannelContext)
+    const [showButtons, setShowButtons] = useState({ display: 'none' });
 
     return (
         <Box py={4} px='2'>
-            <HStack spacing={2} alignItems='flex-start'>
+            <HStack spacing={2} alignItems='center'
+                onMouseEnter={e => {
+                    setShowButtons({ display: 'block' });
+                }}
+                onMouseLeave={e => {
+                    setShowButtons({ display: 'none' })
+                }}>
                 <Avatar name={channelMembers?.[user]?.full_name ?? user} src={channelMembers?.[user]?.user_image} borderRadius={'md'} boxSize='40px' />
                 <Stack spacing='1'>
-                    <HStack divider={<StackDivider />} align='flex-start'>
-                        <Text fontSize='sm' lineHeight={'0.9'} fontWeight="bold" as='span'>{channelMembers?.[user]?.full_name ?? user}</Text>
-                        <Text fontSize="xs" lineHeight={'0.9'} color="gray.500">{DateObjectToTimeString(timestamp)}</Text>
+                    <HStack>
+                        <HStack divider={<StackDivider />} align='flex-start'>
+                            <Text fontSize='sm' lineHeight={'0.9'} fontWeight="bold" as='span'>{channelMembers?.[user]?.full_name ?? user}</Text>
+                            <Text fontSize="xs" lineHeight={'0.9'} color="gray.500">{DateObjectToTimeString(timestamp)}</Text>
+                        </HStack>
+                        <ButtonGroup style={showButtons}>
+                            <IconButton aria-label="edit message" icon={<RiEdit2Line />} size='xs' />
+                            <IconButton aria-label="delete message" icon={<RiDeleteBinLine />} size='xs' />
+                        </ButtonGroup>
                     </HStack>
                     <MarkdownRenderer content={text} />
                 </Stack>
