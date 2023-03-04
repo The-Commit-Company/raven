22ee2c09e8285881f0ecc6d8d94fc5354c647d47
Nikhil Kothari <nik.kothari22@live.com>
2023-03-04 11:02:58 +0530

    chore: mobile app setup with tailwind, ionic

---
+++ mobile/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.raven.app',
+  appName: 'Raven',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ mobile/src/App.tsx
@@ -0,0 +1,78 @@
+import { Redirect, Route } from 'react-router-dom';
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  setupIonicReact
+} from '@ionic/react';
+import { ellipse, square, triangle } from 'ionicons/icons';
+
+import 'tailwindcss/tailwind.css';
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+import './styles/global.css';
+import './styles/variables.css';
+import { IonReactRouter } from '@ionic/react-router';
+
+setupIonicReact({});
+
+function App() {
+
+  return (
+    <IonApp>
+      {/* @ts-ignore */}
+      <IonReactRouter>
+        <IonTabs>
+          <IonRouterOutlet>
+            <Route exact path="/tab1">
+              {/* <Tab1 /> */}
+            </Route>
+            <Route exact path="/tab2">
+              {/* <Tab2 /> */}
+            </Route>
+            <Route path="/tab3">
+              {/* <Tab3 /> */}
+            </Route>
+            <Route exact path="/">
+              <Redirect to="/tab1" />
+            </Route>
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="tab1" href="/tab1">
+              <IonIcon aria-hidden="true" icon={triangle} />
+              <IonLabel>Tab 1</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="tab2" href="/tab2">
+              <IonIcon aria-hidden="true" icon={ellipse} />
+              <IonLabel>Tab 2</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="tab3" href="/tab3">
+              <IonIcon aria-hidden="true" icon={square} />
+              <IonLabel>Tab 3</IonLabel>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonReactRouter>
+    </IonApp>
+  )
+}
+
+export default App
