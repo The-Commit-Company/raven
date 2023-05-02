582d67bafe40b50195ddc4358db5add6c4295e9d
Janhvi Patil <janhvipatil716@gmail.com>
2023-05-02 17:02:07 +0530

    added css to style links in messages

--- raven-app/src/components/feature/markdown-viewer/MarkdownRenderer.tsx
+++ raven-app/src/components/feature/markdown-viewer/MarkdownRenderer.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import rehypeRaw from 'rehype-raw';
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import './MarkdownRenderer.css'
 
 interface Props {
   content: string;
 }
 
 export const MarkdownRenderer: React.FC<Props> = ({ content }) => {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>{content}</ReactMarkdown>
+  return <ReactMarkdown
+    remarkPlugins={[remarkGfm]}
+    rehypePlugins={[rehypeRaw]}
+    className="markdown">
+    {content}
+  </ReactMarkdown>
 }
