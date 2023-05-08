dd2b6a795f87a1d4054db197fe414d4858102b26
Janhvi Patil <janhvipatil716@gmail.com>
2023-05-08 13:02:01 +0530

    function to remove reaction

--- raven/raven_messaging/doctype/raven_message_reaction/raven_message_reaction.py
+++ raven/raven_messaging/doctype/raven_message_reaction/raven_message_reaction.py
@@ -12,8 +12,23 @@ def add_reaction(message_id, reaction, user_id):
 
 	message = frappe.get_doc("Raven Message", message_id)
 
+	# if reaction with user_id already exists, then skip adding reaction
+	for message_reaction in message.message_reaction:
+		if message_reaction.user == user_id and message_reaction.reaction == reaction:
+			return
 	message.append("message_reaction", {
 		"reaction": reaction,
 		"user": user_id
 	})
 	message.save()
+
+@frappe.whitelist(methods=['POST'])
+def remove_reaction(message_id, reaction, user_id):
+
+	message = frappe.get_doc("Raven Message", message_id)
+
+	for message_reaction in message.message_reaction:
+		if message_reaction.user == user_id and message_reaction.reaction == reaction:
+			message.remove(message_reaction)
+			message.save()
+			return
