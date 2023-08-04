ee05ca73b05d1a9a610cf088c777462ee89f4863
Nikhil Kothari <nik.kothari22@live.com>
2023-08-04 15:02:18 +0530

    fix: VirtuosoRefProvider moved down to chat space

--- raven-app/src/pages/MainPage.tsx
+++ raven-app/src/pages/MainPage.tsx
@@ -16,24 +16,24 @@ export const MainPage = (props: Props) => {
 
     return (
         <UserDataProvider>
-            <VirtuosoRefProvider>
-                <Flex height="100vh" sx={{ '--sidebar-width': '16rem' }} >
-                    <Box bg={sidebarBackground} h="100vh" fontSize="sm" width="var(--sidebar-width)" left="0" position="fixed" zIndex="999">
-                        <Stack h="full" direction="column" spacing="4" overflow="auto" {...props}>
-                            <Sidebar isUserActive={isUserActive} />
-                        </Stack>
-                    </Box>
-                    <Box
-                        overflow="auto"
-                        bgColor={chatInterfaceBackground}
-                        w='calc(100vw - var(--sidebar-width))'
-                        position="relative"
-                        left='var(--sidebar-width)'
-                    >
+            <Flex height="100vh" sx={{ '--sidebar-width': '16rem' }} >
+                <Box bg={sidebarBackground} h="100vh" fontSize="sm" width="var(--sidebar-width)" left="0" position="fixed" zIndex="999">
+                    <Stack h="full" direction="column" spacing="4" overflow="auto" {...props}>
+                        <Sidebar isUserActive={isUserActive} />
+                    </Stack>
+                </Box>
+                <Box
+                    overflow="auto"
+                    bgColor={chatInterfaceBackground}
+                    w='calc(100vw - var(--sidebar-width))'
+                    position="relative"
+                    left='var(--sidebar-width)'
+                >
+                    <VirtuosoRefProvider>
                         <Outlet />
-                    </Box>
-                </Flex>
-            </VirtuosoRefProvider>
+                    </VirtuosoRefProvider>
+                </Box>
+            </Flex>
         </UserDataProvider>
     )
 }
