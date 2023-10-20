5ae05a2d17e3c52764429258244f5b261b27b08f
Janhvi Patil <janhvipatil716@gmail.com>
2023-10-20 18:47:19 +0530

    updated user avatar to accept size as optional parameter

--- mobile/src/components/common/UserAvatar.tsx
+++ mobile/src/components/common/UserAvatar.tsx
@@ -1,30 +1,29 @@
 import { IonSkeletonText } from '@ionic/react'
 import avatar from '@/assets/male-avatar.svg'
-import { StyleReactProps } from '@ionic/react/dist/types/components/react-component-lib/interfaces'
-import { useInView } from 'react-intersection-observer';
+import { useInView } from 'react-intersection-observer'
 
 interface AvatarProps {
     src?: string,
     alt: string,
     slot?: string,
     isActive?: boolean,
-    className?: StyleReactProps['className']
+    size?: string,
 }
 const options = {
     root: null,
     rootMargin: "50px",
     threshold: 0.5,
     triggerOnce: true
-};
-export const UserAvatar = ({ src, alt, slot, className, isActive }: AvatarProps) => {
-    const { ref, inView } = useInView(options);
+}
+export const UserAvatar = ({ src, alt, slot, size = '8', isActive }: AvatarProps) => {
+    const { ref, inView } = useInView(options)
     return <span ref={ref} slot={slot} className="relative inline-block">
         {inView ? <img
-            className="h-8 w-8 rounded-md bg-zinc-900 object-cover"
+            className={`h-${size} w-${size} rounded-md bg-zinc-900 object-cover`}
             src={src ? src : avatar}
             alt={alt}
             loading='lazy'
-        /> : <IonSkeletonText animated className='w-8 h-8 rounded-md' />}
+        /> : <IonSkeletonText animated className={`h-${size} w-${size} rounded-md`} />}
         {isActive &&
             <span className="absolute bottom-0 right-0 block translate-x-1/2 translate-y-1/2 transform rounded-full">
                 <span className="block h-2 w-2 rounded-full bg-green-500" />
@@ -32,15 +31,15 @@ export const UserAvatar = ({ src, alt, slot, className, isActive }: AvatarProps)
         }
     </span>
 }
-export const SquareAvatar = ({ src, alt, className, isActive }: AvatarProps) => {
-    const { ref, inView } = useInView(options);
+export const SquareAvatar = ({ src, alt, size = '8', isActive }: AvatarProps) => {
+    const { ref, inView } = useInView(options)
     return <span ref={ref} className="relative inline-block">
         {inView ? <img
-            className="h-8 w-8 rounded-md bg-zinc-900 object-cover"
+            className={`h-${size} w-${size} rounded-md bg-zinc-900 object-cover`}
             src={src ? src : avatar}
             alt={alt}
             loading='lazy'
-        /> : <IonSkeletonText animated className='w-8 h-8 rounded-md' />}
+        /> : <IonSkeletonText animated className={`h-${size} w-${size} rounded-md`} />}
         {isActive &&
             <span className="absolute bottom-0 right-0 block translate-x-1/2 translate-y-1/2 transform rounded-full">
                 <span className="block h-2 w-2 rounded-full bg-green-500" />
