1c481fc3765eddc64e41aee5bef64e187cb0e703
Aditya Patil <patiladitya781@gmail.com>
2023-10-27 15:44:50 +0530

    fix: channel management section UI in channel

--- mobile/src/components/features/channels/ArchiveChannel/ArchiveChannelButton.tsx
+++ mobile/src/components/features/channels/ArchiveChannel/ArchiveChannelButton.tsx
@@ -1,13 +1,19 @@
-import { IonButton } from "@ionic/react"
+import { IonItem, IonLabel } from "@ionic/react"
 import { useState } from "react"
 import { ArchiveChannelAlert } from "."
+import { IoArchiveOutline } from "react-icons/io5"
 
 export const ArchiveChannelButton = ({ channelID }: { channelID: string }) => {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
         <>
-            <IonButton fill="clear" color="danger" onClick={() => setIsOpen(true)} className="text-left justify-start">Archive Channel</IonButton>
+            <IonItem button onClick={() => setIsOpen(true)} detail={false}>
+                <div slot='start'>
+                    <IoArchiveOutline />
+                </div>
+                <IonLabel>Archive Channel</IonLabel>
+            </IonItem>
             <ArchiveChannelAlert isOpen={isOpen} onDismiss={() => setIsOpen(false)} channelID={channelID} />
         </>
     )
--- mobile/src/components/features/channels/DeleteChannel/DeleteChannelButton.tsx
+++ mobile/src/components/features/channels/DeleteChannel/DeleteChannelButton.tsx
@@ -1,13 +1,19 @@
-import { IonButton } from "@ionic/react"
+import { IonItem, IonLabel } from "@ionic/react"
 import { useState } from "react"
 import { DeleteChannelAlert } from "."
+import { IoTrashOutline } from "react-icons/io5"
 
 export const DeleteChannelButton = ({ channelID }: { channelID: string }) => {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
         <>
-            <IonButton fill="clear" color="danger" onClick={() => setIsOpen(true)}>Delete Channel</IonButton>
+            <IonItem button onClick={() => setIsOpen(true)} detail={false}>
+                <div slot='start'>
+                    <IoTrashOutline color='var(--ion-color-danger)' />
+                </div>
+                <IonLabel color='danger'>Delete Channel</IonLabel>
+            </IonItem>
             <DeleteChannelAlert isOpen={isOpen} onDismiss={() => setIsOpen(false)} channelID={channelID} />
         </>
     )
