815fdfda5b83b02fb86d1d6d68bf4d7b97518b47
Janhvi Patil <janhvipatil716@gmail.com>
2023-12-06 22:52:58 +0530

    fix: size of reactions

--- raven-app/src/components/feature/chat/ChatMessage/MessageReactions.tsx
+++ raven-app/src/components/feature/chat/ChatMessage/MessageReactions.tsx
@@ -79,7 +79,7 @@ const ReactionButton = ({ reaction, onReactionClick, currentUser, allUsers }: Re
                 size='1'
                 onClick={onClick}
                 radius='large'
-                className={clsx("w-fit h-full text-sm cursor-pointer hover:bg-[var(--gray-5)] dark:hover:bg-[var(--gray-7)]",
+                className={clsx("w-fit h-full text-xs py-0.5 cursor-pointer hover:bg-[var(--gray-5)] dark:hover:bg-[var(--gray-7)]",
                     currentUserReacted ? "bg-[var(--accent-4)] dark:bg-[var(--accent-a3)]" : "bg-[var(--gray-3)] dark:bg-[var(--gray-5)]")}>
                 <Text as='span' className={clsx("w-fit px-2", currentUserReacted ? "text-[var(--accent-a11)]" : 'text-[var(--gray-12)]')} weight='medium'>
                     {emoji} {count}
