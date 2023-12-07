32b692ff299239aed939ac0fb1a2f6f2ab515980
Nikhil Kothari <nik.kothari22@live.com>
2023-12-07 20:25:28 +0530

    feat: added PWA plugin

--- raven-app/vite.config.ts
+++ raven-app/vite.config.ts
@@ -3,11 +3,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 import proxyOptions from './proxyOptions';
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from 'vite-plugin-pwa'
 
 /// <reference types="vite-plugin-svgr/client" />
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr()],
+	plugins: [react(), svgr(), VitePWA({ registerType: 'autoUpdate' })],
 	server: {
 		port: 8080,
 		proxy: proxyOptions
