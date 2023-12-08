c5fd6f534c23a3563459e82c2dcb2ce2f0fe99fb
Nikhil Kothari <nik.kothari22@live.com>
2023-12-08 22:44:59 +0530

    fix: channel members button

--- raven-app/src/components/feature/chat-header/ViewChannelDetailsButton.tsx
+++ raven-app/src/components/feature/chat-header/ViewChannelDetailsButton.tsx
@@ -6,14 +6,16 @@ import { ChannelMembers } from "@/utils/channel/ChannelMembersProvider"
 import { Button, Dialog, Tooltip } from "@radix-ui/themes"
 import { UserAvatar } from "@/components/common/UserAvatar"
 import { BiSolidUser } from "react-icons/bi"
+import { clsx } from "clsx"
 
 interface ViewChannelDetailsButtonProps {
     channelData: ChannelListItem,
     channelMembers: ChannelMembers,
-    updateMembers: () => void
+    updateMembers: () => void,
+    allowAddMembers: boolean
 }
 
-export const ViewChannelDetailsButton = ({ channelData, channelMembers, updateMembers }: ViewChannelDetailsButtonProps) => {
+export const ViewChannelDetailsButton = ({ channelData, allowAddMembers, channelMembers, updateMembers }: ViewChannelDetailsButtonProps) => {
 
     const [open, setOpen] = useState(false)
 
@@ -26,13 +28,21 @@ export const ViewChannelDetailsButton = ({ channelData, channelMembers, updateMe
 
     return (
         <Dialog.Root open={open} onOpenChange={setOpen}>
-            <Tooltip content='view members/ channel details'>
+            <Tooltip content='view members/channel details'>
                 <Dialog.Trigger>
-                    <Button className={'pr-2 pl-1 w-fit rounded-r-none border-r-0'} variant='surface' color='gray'>
+                    <Button className={clsx('pr-2 pl-1 w-fit cursor-pointer', allowAddMembers ? 'rounded-r-none' : '')}
+                        variant='surface'
+                        color='gray'>
                         {Object.keys(channelMembers).length > 0 ? <div className={'flex -space-x-2 rtl:space-x-reverse'}>
                             {Object.entries(channelMembers).map(([name, member], index) => {
                                 if (index < 3)
-                                    return <UserAvatar key={name} src={member.user_image ?? 'undefined'} alt={member.full_name ?? member.name} radius='full' variant='solid' className={`border border-slate-2`} />
+                                    return <UserAvatar
+                                        key={name}
+                                        src={member.user_image ?? undefined}
+                                        alt={member.full_name ?? member.name}
+                                        radius='full'
+                                        // variant='soft'
+                                        className={`border border-slate-2`} />
                             })}
                             {totalMembers > 3 && <div className={'z-10 flex items-center justify-center w-6 h-6 text-[0.65rem] text-black bg-gray-5 dark:bg-gray-8 dark:text-white rounded-full border border-slate-2'}>+ {totalMembers - 3}</div>}
                         </div> : <BiSolidUser />}
