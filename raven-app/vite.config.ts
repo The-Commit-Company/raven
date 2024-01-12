923c1c639fbab4f3b07b9a9f72ee7e0bda9c7c25
Nikhil Kothari <nik.kothari22@live.com>
2024-01-12 14:49:47 +0530

    fix: manifest for Raven

--- raven-app/vite.config.ts
+++ raven-app/vite.config.ts
@@ -8,7 +8,31 @@ import { VitePWA } from 'vite-plugin-pwa'
 /// <reference types="vite-plugin-svgr/client" />
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr(), VitePWA({ registerType: 'autoUpdate' })],
+	plugins: [react(), svgr(), VitePWA({
+		registerType: 'autoUpdate',
+		devOptions: {
+			enabled: true,
+		},
+		manifest: {
+			display: 'standalone',
+			name: 'Raven',
+			short_name: 'Raven',
+			start_url: '/raven',
+			description: 'Simple work messaging tool',
+			icons: [
+				{
+					src: "/assets/raven/raven/android-chrome-192x192.png",
+					sizes: "192x192",
+					type: "image/png"
+				},
+				{
+					src: "/assets/raven/raven/android-chrome-384x384.png",
+					sizes: "384x384",
+					type: "image/png"
+				}
+			]
+		}
+	})],
 	server: {
 		port: 8080,
 		proxy: proxyOptions
