7c2f8183e8ce907d708c9fdc5a20e613a0281386
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2024-02-23 19:46:12 +0530

    feat: created custom callout component

---
+++ raven-app/src/components/common/Callouts/index.ts
@@ -0,0 +1,5 @@
+export { SuccessCallout } from '@/components/common/Callouts/SuccessCallout'
+export { ErrorCallout } from '@/components/common/Callouts/ErrorCallouts'
+export { CustomCallout } from '@/components/common/Callouts/CustomCallout'
+export type { CalloutObject, CustomCalloutProps } from '@/components/common/Callouts/CustomCallout'
+
