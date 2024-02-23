24c9862437c73a798711d42f9753a3d97b4926be
Sumit Jain <jainmit23@gmail.com>
2024-02-23 18:41:19 +0530

    feat:User, Channel and Channel Type Select Dropdown

--- raven-app/src/components/feature/integrations/webhooks/WebhookForm.tsx
+++ raven-app/src/components/feature/integrations/webhooks/WebhookForm.tsx
@@ -1,13 +1,17 @@
 import React, { useContext } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
-import { Box, Checkbox, Flex, TextFieldInput, Select, TextArea, Heading, Text } from '@radix-ui/themes';
+import { Box, Checkbox, Flex, TextFieldInput, Select, TextArea, Heading, Text, } from '@radix-ui/themes';
 import { ErrorText, HelperText, Label } from '@/components/common/Form';
 import { WebhookData } from './WebhookReturnDataFieldTable';
 import { WebhookHeaders } from './WebhookHeaders';
 import { TriggerEvents } from './utils';
 import { RavenWebhook } from '@/types/RavenIntegrations/RavenWebhook';
-import { UserListContext } from '@/utils/users/UserListProvider';
-import { ChannelListContext, ChannelListContextType } from '@/utils/channel/ChannelListProvider';
+import { UserFields, UserListContext } from '@/utils/users/UserListProvider';
+import { ChannelListContext, ChannelListContextType, ChannelListItem, } from '@/utils/channel/ChannelListProvider';
+import { UserAvatar } from '@/components/common/UserAvatar';
+import { SidebarIcon } from '@/components/layout/Sidebar';
+import { useGetUser } from '@/hooks/useGetUser';
+import { ChannelIcon } from '@/utils/layout/channelIcon';
 
 export const WebhookForm = ({ isEdit = false }: { isEdit?: boolean }) => {
     const { register, formState: { errors }, control, setValue, watch } = useFormContext<RavenWebhook>()
@@ -17,6 +21,10 @@ export const WebhookForm = ({ isEdit = false }: { isEdit?: boolean }) => {
     const needCondition = watch('trigger_webhook_on_condition')
 
     const conditionOn = watch('conditions_on')
+
+    const { users } = useContext(UserListContext)
+
+    const { channels } = useContext(ChannelListContext) as ChannelListContextType
 
     return (
         <Flex direction='column' gap='4' >
@@ -76,8 +84,9 @@ export const WebhookForm = ({ isEdit = false }: { isEdit?: boolean }) => {
                 })} />
                 {errors?.webhook_secret && <ErrorText>{errors.webhook_secret.message}</ErrorText>}
             </Box> : null}
-            <Box>
-                <Flex direction='column' gap='2'>
+            <Box >
+                <Flex direction={'column'}>
+
                     <Label isRequired>Trigger Event</Label>
                     <Controller
                         control={control}
@@ -112,6 +121,17 @@ export const WebhookForm = ({ isEdit = false }: { isEdit?: boolean }) => {
                 <Controller
                     control={control}
                     name='trigger_webhook_on_condition'
+                    rules={{
+                        onChange: (e) => {
+                            if (!e.target.value) {
+                                setValue('conditions_on', '')
+                                setValue('condition', '')
+                                setValue('channel_id', '')
+                                setValue('user', '')
+                                setValue('channel_type', '')
+                            }
+                        }
+                    }}
                     render={({ field }) => (
                         <Flex direction={'row'} gap={'2'} align={'center'}>
                             <Checkbox checked={field.value ? true : false} onClick={() => field.onChange(!field.value)} />
@@ -125,17 +145,28 @@ export const WebhookForm = ({ isEdit = false }: { isEdit?: boolean }) => {
                 {errors?.enable_security && <ErrorText>{errors.enable_security.message}</ErrorText>}
             </Box>
             {needCondition && <Box>
-                <Flex direction={'column'} gap={'2'}>
-                    <Label htmlFor='condition'>Condition On</Label>
+                <Flex direction={'column'}>
+                    <Label htmlFor='condition'>Trigger On</Label>
                     <Controller
                         control={control}
                         name='conditions_on'
+                        rules={{
+                            required: 'Field is required',
+                            onChange: (e) => {
+                                if (e.target.value) {
+                                    setValue('condition', '')
+                                    setValue('channel_id', '')
+                                    setValue('user', '')
+                                    setValue('channel_type', '')
+                                }
+                            }
+                        }}
                         render={({ field }) => (
                             <Select.Root value={field.value} onValueChange={field.onChange} required>
                                 <Select.Trigger placeholder='Select Field' />
                                 <Select.Content>
                                     <Select.Group>
-                                        <Select.Label>Condition On</Select.Label>
+                                        <Select.Label>Trigger On</Select.Label>
                                         <Select.Item value='Channel'>Channel</Select.Item>
                                         <Select.Item value='User'>User</Select.Item>
                                         <Select.Item value='Channel Type' >Channel Type</Select.Item>
@@ -150,14 +181,14 @@ export const WebhookForm = ({ isEdit = false }: { isEdit?: boolean }) => {
             </Box>}
             {conditionOn === 'Custom' ? <Box>
                 <Flex direction={'row'} gap={'8'} align={'start'}>
-                    <Flex direction={'column'} gap={'2'} style={{
+                    <Flex direction={'column'} style={{
                         width: '60%'
                     }} >
                         <Label htmlFor='condition'>Condition</Label>
                         <TextArea {...register('condition')} rows={4} />
                         <HelperText>The webhook will be triggered if this expression is true</HelperText>
                     </Flex>
-                    <Flex direction='column' gap='2' align={'start'}>
+                    <Flex direction='column' align={'start'}>
                         <Heading as='h3' size='2'>Condition Examples :</Heading>
                         <Flex direction='column' gap='1' align={'start'}>
                             <Text size='2'><span>doc.channel_id == 'general'</span></Text>
@@ -166,38 +197,111 @@ export const WebhookForm = ({ isEdit = false }: { isEdit?: boolean }) => {
                     </Flex>
                 </Flex>
             </Box> : conditionOn === 'Channel' ? <Box>
-                <Label htmlFor='channel_id'>Channel ID</Label>
-                <TextFieldInput {...register('channel_id')} />
-                <HelperText>Channel ID on which the condition will be applied</HelperText>
+                    <Flex direction={'column'}>
+                        <Label htmlFor='channel_id'>Channel</Label>
+                        <Controller
+                            control={control}
+                            name='channel_id'
+                            render={({ field }) => (
+                                <Select.Root value={field.value} onValueChange={field.onChange} required>
+                                    <Select.Trigger placeholder='Select Field' />
+                                    <Select.Content>
+                                        <Select.Group>
+                                            <Select.Label>Channel</Select.Label>
+                                            {
+                                                channels.map((channel, index) => (
+                                                    <Select.Item key={index} value={channel.name}>
+                                                        <ChannelItem channel={channel} />
+                                                    </Select.Item>
+                                                ))
+                                            }
+                                        </Select.Group>
+                                    </Select.Content>
+                                </Select.Root>
+                            )}
+                        />
+                        <HelperText>Channel ID on which the condition will be applied</HelperText>
+                    </Flex>
             </Box> : conditionOn === 'User' ? <Box>
-                <Label htmlFor='user_id'>User ID</Label>
-                <TextFieldInput {...register('user')} />
-                <HelperText>User ID on which the condition will be applied</HelperText>
+                        <Flex direction={'column'} >
+                            <Label htmlFor='user_id'>User</Label>
+                            <Controller
+                                control={control}
+                                name='user'
+                                render={({ field }) => (
+                                    <Select.Root value={field.value} onValueChange={field.onChange} required>
+                                        <Select.Trigger placeholder='Select Field' />
+                                        <Select.Content>
+                                            <Select.Group>
+                                                <Select.Label>User</Select.Label>
+                                                {
+                                                    users.map((user, index) => (
+                                                        <Select.Item key={index} value={user.name}>
+                                                            <DirectMessageItem user={user} />
+                                                        </Select.Item>
+                                                    ))
+                                                }
+                                            </Select.Group>
+                                        </Select.Content>
+                                    </Select.Root>
+                                )}
+                            />
+                            <HelperText>User ID on which the condition will be applied</HelperText>
+                        </Flex>
             </Box> : conditionOn === 'Channel Type' ? <Box>
-                <Label htmlFor='channel_type'>Channel Type</Label>
-                <Controller
-                    control={control}
-                    name='channel_type'
-                    render={({ field }) => (
-                        <Select.Root value={field.value} onValueChange={field.onChange} required>
-                            <Select.Trigger placeholder='Select Field' />
-                            <Select.Content>
-                                <Select.Group>
-                                    <Select.Label>Channel Type</Select.Label>
-                                    <Select.Item value='Public'>Public</Select.Item>
-                                    <Select.Item value='Private'>Private</Select.Item>
-                                    <Select.Item value='Open'>Open</Select.Item>
-                                    <Select.Item value='DM'>Direct Message</Select.Item>
-                                    <Select.Item value='Self Message'>Self Message</Select.Item>
-                                </Select.Group>
-                            </Select.Content>
-                        </Select.Root>
-                    )}
-                />
-                <HelperText>Channel Type on which the condition will be applied</HelperText>
+                            <Flex direction={'column'} >
+                                <Label htmlFor='channel_type'>Channel Type</Label>
+                                <Controller
+                                    control={control}
+                                    name='channel_type'
+                                    render={({ field }) => (
+                                        <Select.Root value={field.value} onValueChange={field.onChange} required>
+                                            <Select.Trigger placeholder='Select Field' />
+                                            <Select.Content>
+                                                <Select.Group>
+                                                    <Select.Label>Channel Type</Select.Label>
+                                                    <Select.Item value='Public'>Public</Select.Item>
+                                                    <Select.Item value='Private'>Private</Select.Item>
+                                                    <Select.Item value='Open'>Open</Select.Item>
+                                                    <Select.Item value='DM'>Direct Message</Select.Item>
+                                                    <Select.Item value='Self Message'>Self Message</Select.Item>
+                                                </Select.Group>
+                                            </Select.Content>
+                                        </Select.Root>
+                                    )}
+                                />
+                                <HelperText>Channel Type on which the condition will be applied</HelperText>
+                            </Flex>
             </Box> : null}
-            <WebhookData />
-            <WebhookHeaders />
+            <Flex direction={'column'} gap={'4'} py={'2'} >
+                <WebhookData />
+                <WebhookHeaders />
+            </Flex>
         </Flex>
     )
+}
+
+const DirectMessageItem = ({ user }: { user: UserFields }) => {
+
+    const userData = useGetUser(user?.name)
+
+    return <Flex direction={'row'} gap={'2'} align={'center'}>
+        <SidebarIcon>
+            <UserAvatar src={userData?.user_image} alt={userData?.full_name} size='1' />
+        </SidebarIcon>
+        <Flex justify='between'>
+            <Text size='2' className="text-ellipsis line-clamp-1" >
+                {userData?.full_name ?? user?.name}
+            </Text>
+        </Flex>
+    </Flex>
+}
+
+const ChannelItem = ({ channel }: { channel: ChannelListItem }) => {
+    return <Flex direction={'row'} gap={'2'} align={'center'}>
+        <ChannelIcon type={channel.type} size='18' />
+        <Flex justify='between' align={'center'} width='100%'>
+            <Text size='2' className="text-ellipsis line-clamp-1" as='span' >{channel.channel_name}</Text>
+        </Flex>
+    </Flex>
 }
