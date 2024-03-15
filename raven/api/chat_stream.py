9b3b1489a4d9d9780d6de6f89ba8a9bb3ae54cc1
Nikhil Kothari <nik.kothari22@live.com>
2024-03-15 23:15:42 +0530

    tests: added tests for chat stream APIs

--- raven/api/chat_stream.py
+++ raven/api/chat_stream.py
@@ -1,20 +1,22 @@
 import frappe
-from frappe.query_builder.functions import Count, Coalesce
-from frappe.query_builder import Case, Order, JoinType
+from frappe.query_builder import Order
 from raven.api.raven_message import track_visit
+import datetime
 
 @frappe.whitelist()
-def get_messages(channel_id: str, limit: int = 20):
+def get_messages(channel_id: str, limit: int = 20, base_message: str | None = None):
     '''
         API to get list of messages for a channel, ordered by creation date (newest first)
     
     '''
+    
     # Check permission for channel access
     if not frappe.has_permission(doctype='Raven Channel', doc=channel_id, ptype='read'):
         frappe.throw('You do not have permission to access this channel', frappe.PermissionError)
 
-
     # Fetch messages for the channel
+    if base_message:
+        return get_messages_around_base(channel_id, base_message)
 
     # Cannot use `get_all` as it does not apply the `order_by` clause to multiple fields
     message = frappe.qb.DocType('Raven Message')
@@ -26,7 +28,7 @@ def get_messages(channel_id: str, limit: int = 20):
         message.replied_message_details, message.content, message.is_edited
     ).where(
         message.channel_id == channel_id
-    ).orderby(message.creation, order=Order.desc).orderby(message.name, order=Order.asc).limit(limit).run(as_dict=True)
+    ).orderby(message.creation, order=Order.desc).orderby(message.name, order=Order.desc).limit(limit).run(as_dict=True)
     
     has_old_messages = False
 
@@ -36,7 +38,7 @@ def get_messages(channel_id: str, limit: int = 20):
         older_message = frappe.db.get_all('Raven Message',
                                           pluck='name',
                                             filters={'channel_id': channel_id, 'creation': ('<', messages[-1].creation)},
-                                            order_by='creation desc, name asc',
+                                            order_by='creation desc, name desc',
                                             limit=1
                                             )
         
@@ -50,19 +52,46 @@ def get_messages(channel_id: str, limit: int = 20):
         'has_new_messages': False
     }
 
+def get_messages_around_base(channel_id: str, base_message: str):
+    '''
+        Get 10 messages before base message and 10 messages after
+    '''
+       # Fetch older messages for the channel
+    from_timestamp = frappe.get_cached_value('Raven Message', base_message, 'creation')
+
+    # TODO: Optimize this to use a complex SQL query to fetch around the main message
+    older_messages = fetch_older_messages(channel_id, base_message, from_timestamp, 10)
+    newer_messages = fetch_newer_messages(channel_id, base_message, from_timestamp, 10, include_from_message=True)
+
+    combined_messages = newer_messages.get("messages", []) + older_messages.get("messages", [])
+
+    return {
+        **older_messages,
+        **newer_messages,
+        "messages": combined_messages,
+        "from_timestamp": from_timestamp
+    }
+
+
 @frappe.whitelist()
 def get_older_messages(channel_id: str, from_message: str, limit: int = 20):
     '''
         API to get older messages for a channel, ordered by creation date (newest first)
+
+        Function is split into two to avoid duplicate perm check and timestamp check
     '''
 
     # Check permission for channel access
     if not frappe.has_permission(doctype='Raven Channel', doc=channel_id, ptype='read'):
         frappe.throw('You do not have permission to access this channel', frappe.PermissionError)
-
     # Fetch older messages for the channel
     from_timestamp = frappe.get_cached_value('Raven Message', from_message, 'creation')
 
+    return fetch_older_messages(channel_id, from_message, from_timestamp, limit)
+
+    
+
+def fetch_older_messages(channel_id: str, from_message: str, from_timestamp: datetime.datetime, limit: int = 20):
     # Cannot use `get_all` as it does not apply the `order_by` clause to multiple fields
     message = frappe.qb.DocType('Raven Message')
 
@@ -73,7 +102,7 @@ def get_older_messages(channel_id: str, from_message: str, limit: int = 20):
         message.replied_message_details, message.content, message.is_edited
     ).where(
         message.channel_id == channel_id
-    ).where(message.creation < from_timestamp or (message.creation == from_timestamp and message.name > from_message)).orderby(message.creation, order=Order.desc).orderby(message.name, order=Order.asc).limit(limit).run(as_dict=True)
+    ).where((message.creation < from_timestamp) | ((message.creation == from_timestamp) & (message.name < from_message))).orderby(message.creation, order=Order.desc).orderby(message.name, order=Order.desc).limit(limit).run(as_dict=True)
 
     has_old_messages = False
 
@@ -82,8 +111,8 @@ def get_older_messages(channel_id: str, from_message: str, limit: int = 20):
         # Check if there are more messages available
         older_message = frappe.db.get_all('Raven Message',
                                           pluck='name',
-                                            filters={'channel_id': channel_id, 'creation': ('<', messages[-1].creation)},
-                                            order_by='creation desc, name asc',
+                                            filters={'channel_id': channel_id, 'creation': ('<=', messages[-1].creation), 'name': ['!=', messages[-1].name]},
+                                            order_by='creation desc, name desc',
                                             limit=1
                                             )
         
@@ -108,8 +137,17 @@ def get_newer_messages(channel_id: str, from_message: str, limit: int = 20):
     # Fetch older messages for the channel
     from_timestamp = frappe.get_cached_value('Raven Message', from_message, 'creation')
 
-    # Cannot use `get_all` as it does not apply the `order_by` clause to multiple fields
+    return fetch_newer_messages(channel_id, from_message, from_timestamp, limit, include_from_message=False)
+
+def fetch_newer_messages(channel_id: str, from_message: str, from_timestamp: datetime.datetime, limit: int = 20, include_from_message: bool = False):
+
     message = frappe.qb.DocType('Raven Message')
+
+    if include_from_message:
+        condition = ((message.creation > from_timestamp) | ((message.creation == from_timestamp) & (message.name >= from_message)))
+    
+    else:
+        condition = ((message.creation > from_timestamp) | ((message.creation == from_timestamp) & (message.name > from_message)))
 
     messages = frappe.qb.from_(message).select(
         message.name, message.owner, message.creation, message.modified, message.text,
@@ -118,23 +156,25 @@ def get_newer_messages(channel_id: str, from_message: str, limit: int = 20):
         message.replied_message_details, message.content, message.is_edited
     ).where(
         message.channel_id == channel_id
-    ).where(message.creation > from_timestamp or (message.creation == from_timestamp and message.name < from_message)).orderby(message.creation, order=Order.desc).orderby(message.name, order=Order.asc).limit(limit).run(as_dict=True)
+    ).where(condition).orderby(message.creation, order=Order.asc).orderby(message.name, order=Order.asc).limit(limit).run(as_dict=True)
 
     has_new_messages = False
 
-    # Check if older messages are available
+    # Check if newer messages are available
     if len(messages) == limit:
         # Check if there are more messages available
         newer_message = frappe.db.get_all('Raven Message',
                                           pluck='name',
-                                            filters={'channel_id': channel_id, 'creation': ('>', messages[-1].creation)},
-                                            order_by='creation desc, name asc',
+                                            filters={'channel_id': channel_id, 'creation': ('>=', messages[-1].creation), 'name': ['!=', messages[-1].name]},
+                                            order_by='creation asc, name asc',
                                             limit=1
                                             )
         
         if len(newer_message) > 0:
             has_new_messages = True
     
+    # The messages are in ascending order, so reverse them
+    messages.reverse()
     return {
         'messages': messages,
         'has_new_messages': has_new_messages
