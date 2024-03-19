81850632395c063117c7edbd48456bc84b8d1b31
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2024-03-19 13:22:44 +0530

    chore(mobile): add Inter font as default font family

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -72,6 +72,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        'sans': ['Inter', 'sans-serif'],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
