198e1d80daf64664527cb7752beaa668c1bf98f0
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2024-03-26 19:56:39 +0530

    fix(mobile): minor ui changes in chat

--- mobile/src/components/features/chat-space/chat-view/DateSeparator.tsx
+++ mobile/src/components/features/chat-space/chat-view/DateSeparator.tsx
@@ -1,5 +1,4 @@
 import { DateObjectToFormattedDateString } from '@/utils/operations/operations'
-import { IonText } from '@ionic/react'
 
 type Props = {
     date: string
@@ -11,13 +10,10 @@ const parseDateString = (date: string) => {
 }
 export const DateSeparator = ({ date }: Props) => {
     return (
-        <div className='relative my-2 px-2'>
-            <div className="absolute inset-0 flex items-center mx-4" aria-hidden="true">
-                <div className="w-full border-t border-t-zinc-800" />
-            </div>
-            <div className="relative flex justify-center">
-                <IonText className="px-2 text-sm bg-[var(--ion-background-color)] font-bold text-zinc-300">{parseDateString(date)}</IonText>
-            </div>
+        <div className='relative flex items-center my-2 px-2'>
+            <div className="grow border-t border-foreground/20"></div>
+            <span className="shrink px-2 text-xs font-semibold text-foreground/80">{parseDateString(date)}</span>
+            <div className="grow border-t border-foreground/20"></div>
         </div>
     )
 }
--- mobile/src/components/features/chat-space/chat-view/MessageBlock.tsx
+++ mobile/src/components/features/chat-space/chat-view/MessageBlock.tsx
@@ -57,9 +57,9 @@ export const NonContinuationMessageBlock = ({ message, user, onMessageSelect }: 
         <div className='px-2 mt-3 py-1 rounded-md flex active:bg-[color:var(--ion-color-light)]' {...longPressEvent}>
             <UserAvatarBlock message={message} user={user} />
             <div>
-                <div className='flex items-end'>
-                    <IonText className='font-black text-sm'>{user?.full_name ?? message.data.owner}</IonText>
-                    <IonText className='text-xs pl-1.5 text-zinc-500'>{DateObjectToTimeString(message.data.creation)}</IonText>
+                <div className='flex items-baseline'>
+                    <p className="text-foreground text-sm font-medium leading-normal tracking-normal">{user?.full_name ?? message.data.owner}</p>
+                    <p className='text-xs pl-1.5 text-foreground/50'>{DateObjectToTimeString(message.data.creation)}</p>
                 </div>
                 <MessageContent message={message} />
                 {message.data.is_edited === 1 && <IonText className='text-xs' color={'medium'}>(edited)</IonText>}
@@ -112,7 +112,7 @@ const MessageContent = ({ message }: { message: MessageBlock }) => {
 
     return <div className='min-w-[100px] max-w-[280px]'>
         {message.data.is_reply === 1 && message.data.linked_message && message.data.replied_message_details && <ReplyBlock message={JSON.parse(message.data.replied_message_details)} />}
-        {message.data.message_type === 'Text' && <div className='text-zinc-100 text-md'><TextMessageBlock message={message.data} /></div>}
+        {message.data.message_type === 'Text' && <div className='text-foreground text-sm font-normal leading-normal tracking-normal'><TextMessageBlock message={message.data} /></div>}
         {message.data.message_type === 'Image' && <ImageMessageBlock message={message.data} />}
         {message.data.message_type === 'File' && <FileMessageBlock message={message.data} />}
     </div>
@@ -211,11 +211,11 @@ const ReplyBlock = ({ message }: { message: Message }) => {
     const date = message ? new Date(message?.creation) : null
     return <div onClick={scrollToMessage} className='px-2 py-1.5 my-2 rounded-e-sm bg-neutral-900 border-l-4 border-l-neutral-500'>
         {message && <div>
-            <div className='flex items-end pb-1'>
-                <IonText className='font-bold text-sm'>{user?.full_name ?? message.owner}</IonText>
-                {date && <IonText className='font-normal text-xs pl-2' color='medium'>on {DateObjectToFormattedDateStringWithoutYear(date)} at {DateObjectToTimeString(date)}</IonText>}
+            <div className='flex items-baseline pb-1'>
+                <p className='text-foreground font-medium text-sm leading-normal tracking-normal'>{user?.full_name ?? message.owner}</p>
+                {date && <p className='text-xs text-foreground/60 font-normal pl-1.5'>on {DateObjectToFormattedDateStringWithoutYear(date)} at {DateObjectToTimeString(date)}</p>}
             </div>
-            {message.message_type === 'Text' && <div className='text-sm text-neutral-400 line-clamp-3'>{parse(message.content ?? '')}</div>}
+            {message.message_type === 'Text' && <div className='text-sm text-foreground/60 line-clamp-3'>{parse(message.content ?? '')}</div>}
             {message.message_type === 'Image' && <div className='flex items-center space-x-2'>
                 <img src={message.file} alt={`Image`} className='inline-block w-10 h-10 rounded-md' />
                 <p className='text-sm font-semibold'>📸 &nbsp;Image</p>
