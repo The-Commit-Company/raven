0fd1e889a0ad5826c9e7a888d1be1bd4e3abb142
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2024-06-13 01:16:01 +0530

    feat: create doctype preview component

--- raven-app/src/components/feature/chat/ChatMessage/Renderers/DoctypeLinkRenderer.tsx
+++ raven-app/src/components/feature/chat/ChatMessage/Renderers/DoctypeLinkRenderer.tsx
@@ -1,14 +1,17 @@
-import { Flex, IconButton, Link } from "@radix-ui/themes"
+import { useDoctypePreview } from "@/hooks/useDoctypePreview"
+import { Flex, Heading, IconButton, Tooltip } from "@radix-ui/themes"
 import { FrappeConfig, FrappeContext } from "frappe-react-sdk"
 import { useContext } from "react"
-import { BiLink, BiRightArrowAlt } from "react-icons/bi"
-import { FiExternalLink } from "react-icons/fi"
+import { Grid, Text, Box, Card } from "@radix-ui/themes"
 import { toast } from "sonner"
+import { BiCopy } from "react-icons/bi"
+import { FiExternalLink } from "react-icons/fi"
 
 
 export const DoctypeLinkRenderer = ({ doctype, docname }: { doctype: string, docname: string }) => {
 
     const { call } = useContext(FrappeContext) as FrappeConfig
+    const { data, error, isLoading } = useDoctypePreview(doctype, docname)
 
     const getRoute = async (doctype: string, docname: string): Promise<string> => {
 
@@ -27,6 +30,16 @@ export const DoctypeLinkRenderer = ({ doctype, docname }: { doctype: string, doc
 
     }
 
+    const removePreviewFields = (data: Record<string, any>) => {
+        const fieldsToRemove = ['preview_image', 'preview_title', 'id']
+        return Object.keys(data).reduce((acc, key) => {
+            if (!fieldsToRemove.includes(key)) {
+                acc[key as keyof typeof data] = data[key]
+            }
+            return acc
+        }, {} as Record<string, any>)
+    }
+
     const onCopyClick = () => {
         toast.promise(copyLink, {
             loading: 'Copying link...',
@@ -34,6 +47,8 @@ export const DoctypeLinkRenderer = ({ doctype, docname }: { doctype: string, doc
             error: 'Failed to copy link'
         })
     }
+
+    const copyToClipboard = (text: string) => navigator.clipboard.writeText(text)
 
     const copyLink = async () => {
 
@@ -49,38 +64,97 @@ export const DoctypeLinkRenderer = ({ doctype, docname }: { doctype: string, doc
     }
 
     return (
-        <Flex
-            align='center'
-            gap='4'
-            mt={'1'}
-            p='2'
-            className="border-2 bg-gray-2 dark:bg-gray-4 rounded-md border-gray-4  dark:border-gray-6 shadow-sm">
-            <Flex align='center' gap='2'>
-                <BiRightArrowAlt />
-                <Link size='2' underline="always" onClick={openLink}>{doctype}: {docname}</Link>
-            </Flex>
+        <Box className="max-w-[550px] min-w-[75px]">
+            <Card>
+                <Grid gap="2">
+                    <Flex gap="4" align="center">
+                        {
+                            !data?.preview_image ?
+                                <Box>
+                                    <img
+                                        src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+                                        alt="Bold typography"
+                                        style={{
+                                            objectFit: 'cover',
+                                            width: '100%',
+                                            minWidth: '50px',
+                                            minHeight: '50px',
+                                            maxWidth: '100px',
+                                            maxHeight: '100px',
+                                            backgroundColor: 'var(--gray-5)',
+                                            borderRadius: 'var(--radius-4)'
+                                        }}
+                                    />
+                                </Box>
+                                : null
+                        }
+                        <Flex justify="between" className="flex-1">
+                            <Grid gap="0">
+                                <Heading as="h3" size="4">{data.preview_title}</Heading>
+                                <Text
+                                    size="2"
+                                    color="gray"
+                                    className="cursor-copy"
+                                    onClick={() => copyToClipboard(data.id)}>
+                                    {data.id}
+                                </Text>
+                            </Grid>
+                            <Flex gap="2" align="center">
+                                <Tooltip content="Open in new tab">
+                                    <IconButton
+                                        size={'1'}
+                                        title="Open in new tab"
+                                        color='gray'
+                                        onClick={openLink}
+                                        variant="soft"
+                                    >
+                                        <FiExternalLink />
+                                    </IconButton>
+                                </Tooltip>
 
-            <Flex align='center' gap='2'>
-                <IconButton
-                    size={'1'}
-                    title="Open in new tab"
-                    color='gray'
-                    onClick={openLink}
-                    variant="soft"
-                >
-                    <FiExternalLink />
-                </IconButton>
+                                <Tooltip content="Copy link">
+                                    <IconButton
+                                        size='1'
+                                        title="Copy link"
+                                        color='gray'
+                                        onClick={onCopyClick}
+                                        variant="soft"
+                                    >
+                                        <BiCopy />
+                                    </IconButton>
+                                </Tooltip>
+                            </Flex>
+                        </Flex>
+                    </Flex>
 
-                <IconButton
-                    size='1'
-                    title="Copy link"
-                    color='gray'
-                    onClick={onCopyClick}
-                    variant="soft"
-                >
-                    <BiLink />
-                </IconButton>
-            </Flex>
-        </Flex>
+                    <Grid gap="2" >
+                        {
+                            Object.keys(removePreviewFields(data))?.map((item, index) => (
+                                <Flex key={item + index} gap="4" align="center">
+                                    <Flex>
+                                        <Text
+                                            size="2"
+                                            weight="bold"
+                                            color="gray"
+                                        >
+                                            {item}
+                                        </Text>
+                                    </Flex>
+                                    <Flex gap="2" align="center">
+                                        <Text
+                                            size="2"
+                                            className="cursor-copy"
+                                            onClick={() => copyToClipboard(data[item])}
+                                        >
+                                            {data[item]}
+                                        </Text>
+                                    </Flex>
+                                </Flex>
+                            ))
+                        }
+                    </Grid>
+                </Grid>
+            </Card>
+        </Box>
     )
 }
