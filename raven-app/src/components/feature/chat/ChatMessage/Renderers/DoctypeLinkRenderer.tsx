c89359f9c1814af7fffc967006a885a8a0ba01e5
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2024-06-13 13:49:41 +0530

    refactor: comment fixes doctype preview

--- raven-app/src/components/feature/chat/ChatMessage/Renderers/DoctypeLinkRenderer.tsx
+++ raven-app/src/components/feature/chat/ChatMessage/Renderers/DoctypeLinkRenderer.tsx
@@ -4,8 +4,7 @@ import { FrappeConfig, FrappeContext } from "frappe-react-sdk"
 import { useContext } from "react"
 import { Grid, Text, Box, Card } from "@radix-ui/themes"
 import { toast } from "sonner"
-import { BiCopy } from "react-icons/bi"
-import { FiExternalLink } from "react-icons/fi"
+import { BiCopy, BiLinkExternal } from "react-icons/bi"
 
 
 export const DoctypeLinkRenderer = ({ doctype, docname }: { doctype: string, docname: string }) => {
@@ -44,17 +43,17 @@ export const DoctypeLinkRenderer = ({ doctype, docname }: { doctype: string, doc
     }
 
     return (
-        <Box className="max-w-[550px] min-w-[75px]">
+        <Box className='max-w-[550px] min-w-[75px]'>
             {
                 isLoading ?
                     <Skeleton className='w-96 h-12 rounded-md' /> :
                     error ?
                         <Card>
-                            <Grid gap="2" align="center">
-                                <Heading as="h3" size="4">Error occurred while loading Doctype</Heading>
+                            <Grid gap='2' align='center'>
+                                <Heading as='h3' size='4'>Error occurred while loading Doctype</Heading>
                                 <Text
-                                    size="2"
-                                    color="gray"
+                                    size='2'
+                                    color='gray'
                                 >
                                     {error.message}
                                 </Text>
@@ -69,6 +68,7 @@ export const DoctypeLinkRenderer = ({ doctype, docname }: { doctype: string, doc
 
 const DoctypeCard = ({ data, copyLink, openLink }: { data: Record<string, any>, copyLink: () => Promise<void>, openLink: () => Promise<void> }) => {
 
+    // utility func to remove known preview fields in order to map rest of them
     const removePreviewFields = (data: Record<string, any>) => {
         const fieldsToRemove = ['preview_image', 'preview_title', 'id']
         return Object.keys(data).reduce((acc, key) => {
@@ -100,8 +100,8 @@ const DoctypeCard = ({ data, copyLink, openLink }: { data: Record<string, any>, 
 
     return (
         <Card>
-            <Grid gap="2">
-                <Flex gap="4" align="center">
+            <Grid gap='2'>
+                <Flex gap='4' align='center'>
                     {
                         data?.preview_image &&
                         <Box>
@@ -121,38 +121,38 @@ const DoctypeCard = ({ data, copyLink, openLink }: { data: Record<string, any>, 
                             />
                         </Box>
                     }
-                    <Flex justify="between" className="flex-1">
-                        <Grid gap="0">
-                            <Heading as="h3" size="4">{data?.preview_title}</Heading>
+                    <Flex justify='between' className='flex-1'>
+                        <Grid gap='0'>
+                            <Heading as='h3' size='4'>{data?.preview_title}</Heading>
                             <Text
-                                size="2"
-                                color="gray"
-                                className="cursor-copy"
-                                onClick={() => onCopyTextClick("ID")}
+                                size='2'
+                                color='gray'
+                                className='cursor-copy'
+                                onClick={() => onCopyTextClick('ID')}
                             >
                                 {data?.id}
                             </Text>
                         </Grid>
-                        <Flex gap="2" align="center">
-                            <Tooltip content="Open in new tab">
+                        <Flex gap='2' align='center'>
+                            <Tooltip content='Open in new tab'>
                                 <IconButton
-                                    size={'1'}
-                                    title="Open in new tab"
+                                    size='1'
+                                    title='Open in new tab'
                                     color='gray'
                                     onClick={openLink}
-                                    variant="soft"
+                                    variant='soft'
                                 >
-                                    <FiExternalLink />
+                                    <BiLinkExternal />
                                 </IconButton>
                             </Tooltip>
 
-                            <Tooltip content="Copy link">
+                            <Tooltip content='Copy link'>
                                 <IconButton
                                     size='1'
-                                    title="Copy link"
+                                    title='Copy link'
                                     color='gray'
                                     onClick={onCopyLinkClick}
-                                    variant="soft"
+                                    variant='soft'
                                 >
                                     <BiCopy />
                                 </IconButton>
@@ -161,23 +161,23 @@ const DoctypeCard = ({ data, copyLink, openLink }: { data: Record<string, any>, 
                     </Flex>
                 </Flex>
 
-                <Grid gap="2" >
+                <Grid gap='2' >
                     {
                         data && Object.keys(removePreviewFields(data))?.map((item, index) => (
-                            <Flex key={item + index} gap="4" align="center">
+                            <Flex key={item + index} gap='4' align='center'>
                                 <Flex>
                                     <Text
-                                        size="2"
-                                        weight="bold"
-                                        color="gray"
+                                        size='2'
+                                        weight='bold'
+                                        color='gray'
                                     >
                                         {item}
                                     </Text>
                                 </Flex>
-                                <Flex gap="2" align="center">
+                                <Flex gap='2' align='center'>
                                     <Text
-                                        size="2"
-                                        className="cursor-copy"
+                                        size='2'
+                                        className='cursor-copy'
                                         onClick={() => onCopyTextClick(item)}
                                     >
                                         {data[item]}
--- raven-app/src/hooks/useDoctypePreview.ts
+++ raven-app/src/hooks/useDoctypePreview.ts
@@ -2,7 +2,7 @@ import { useFrappeGetCall } from "frappe-react-sdk"
 
 /**
  * Simple hook to show Doctype preview data
- * @returns Description, Item Group, name(id), preview_image, preview_title
+ * @returns name(id), preview_image, preview_title and rest fields depending on Doctype
  */
 export const useDoctypePreview = (doctype: string, docname: string) => {
     const {data, error, isLoading} = useFrappeGetCall('raven.api.document_link.get_preview_data', {
