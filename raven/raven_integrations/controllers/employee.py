fe43b716130fc99508486e7a65a960a705e8d184
Nikhil Kothari <nik.kothari22@live.com>
2024-06-27 18:15:37 +0530

    feat: sync employee data with channel members

--- raven/raven_integrations/controllers/employee.py
+++ raven/raven_integrations/controllers/employee.py
@@ -4,9 +4,13 @@ def after_insert(doc, method):
     """
     Create a new Channel Member if a channel for the department exists
     """
+
     if not doc.department or not doc.user_id:
         return
-
+    
+    if not is_department_sync_on():
+        return
+    
     channel_id = get_channel_for_department(doc.department)
 
     if not channel_id:
@@ -20,10 +24,44 @@ def after_insert(doc, method):
     create_channel_member(channel_id=channel_id, raven_user_id=raven_user_id, employee_id=doc.name)
 
 def on_update(doc, method):
-    # TODO: Check if department has changed. 
+    # Check if department has changed. 
     # If it has, we need to remove the channel member from the existing department channel
     # And add it to the new department's channel
-    pass
+
+    if not doc.user_id:
+        return
+
+    if not is_department_sync_on():
+        return
+    
+    if doc.has_value_changed('user_id'):
+        # TODO: Create a member for this user_id
+        pass
+    
+    if doc.has_value_changed('department'):
+
+        raven_user_id = get_raven_user_for_user(doc.user_id)
+    
+        if not raven_user_id:
+            return
+        
+        old_doc = doc.get_doc_before_save()
+
+        if old_doc.department:
+            # Remove the employee as a member from the channel
+            old_channel = get_channel_for_department(old_doc.department)
+
+            if old_channel:
+                frappe.db.delete("Raven Channel Member", {
+                    "channel_id": old_channel,
+                    "user_id": raven_user_id
+                })
+        
+        # Create a new Raven Channel Member for employee
+        new_channel = get_channel_for_department(doc.department)
+
+        if new_channel:
+            create_channel_member(channel_id=new_channel, raven_user_id=raven_user_id, employee_id=doc.name)
 
 def on_trash(doc, method):
     frappe.db.delete("Raven Channel Member", {
@@ -42,7 +80,7 @@ def create_channel_member(channel_id, raven_user_id, employee_id):
     })
 
     channel_member.insert(ignore_permissions=True)
-
+    
 def get_channel_for_department(department):
      # Check if channel exists for the department
     channel = frappe.get_all("Raven Channel", {
@@ -65,3 +103,12 @@ def get_raven_user_for_user(user_id):
         return None
     
     return raven_user[0]
+
+def is_department_sync_on():
+
+    raven_settings = frappe.get_single("Raven Settings")
+
+    if raven_settings.auto_create_department_channel:
+        return True
+    else:
+        return False
