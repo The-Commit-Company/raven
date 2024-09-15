be714f36d9e1081eb3ee01c5b2c12cc93da0b969
Samarth Upare <samarth.upare@redtra.com>
2024-09-16 04:22:35 +0530

    sid logic changes added

--- frontend/src/utils/auth/ProtectedRoute.tsx
+++ frontend/src/utils/auth/ProtectedRoute.tsx
@@ -1,19 +1,38 @@
-import { useContext } from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-import { FullPageLoader } from '../../components/layout/Loaders'
-import { UserContext } from './UserProvider'
+import { useContext, useEffect, useState } from 'react';
+import { Navigate, Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { FullPageLoader } from '../../components/layout/Loaders';
+import { UserContext } from './UserProvider';
 
 export const ProtectedRoute = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const { currentUser, isLoading } = useContext(UserContext);
 
-    const { currentUser, isLoading } = useContext(UserContext)
+    // State to manage if the page has been reloaded
+    const [reloaded, setReloaded] = useState(false);
 
+    // Effect to handle the reload based on 'sid' presence in the URL
+    useEffect(() => {
+        const searchParams = new URLSearchParams(location.search);
+        const sid = searchParams.get('sid');
+
+        // Check if 'sid' is present and the page has not been reloaded yet
+        if (sid && !reloaded) {
+            setReloaded(true); // Set to true to prevent future reloads
+            navigate(location.pathname, { replace: true }); // Reload the page using only pathname
+        }
+    }, [location, navigate, reloaded]);
+
+    // Handle loading state
     if (isLoading) {
-        return <FullPageLoader />
+        return <FullPageLoader />;
     }
-    else if (!currentUser || currentUser === 'Guest') {
-        return <Navigate to="/login" />
+
+    // Redirect if user is not authenticated or is a 'Guest'
+    if (!currentUser || currentUser === 'Guest') {
+        return <Navigate to="/login" />;
     }
-    return (
-        <Outlet />
-    )
-}
+
+    // Render the outlet if all checks pass
+    return <Outlet />;
+};
