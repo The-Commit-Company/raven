121c999c992d34fd32962b9af2dcb9e9ae1d94db
Nikhil Kothari <nik.kothari22@live.com>
2024-09-17 07:39:36 +0530

    fix: remove Raven from navbar items during install

--- raven/install.py
+++ raven/install.py
@@ -1,12 +1,10 @@
 import click
-import frappe
 from frappe.desk.page.setup_wizard.setup_wizard import make_records
 
 
 def after_install():
 	try:
 		print("Setting up Raven...")
-		add_standard_navbar_items()
 		create_general_channel()
 
 		click.secho("Thank you for installing Raven!", fg="green")
@@ -28,39 +26,3 @@ def create_general_channel():
 	]
 
 	make_records(channel)
-
-
-def add_standard_navbar_items():
-	navbar_settings = frappe.get_single("Navbar Settings")
-
-	raven_navbar_items = [
-		{
-			"item_label": "Raven",
-			"item_type": "Route",
-			"route": "/raven",
-			"is_standard": 1,
-		}
-	]
-
-	current_navbar_items = navbar_settings.settings_dropdown
-	navbar_settings.set("settings_dropdown", [])
-
-	for item in raven_navbar_items:
-		current_labels = [item.get("item_label") for item in current_navbar_items]
-		if not item.get("item_label") in current_labels:
-			navbar_settings.append("settings_dropdown", item)
-
-	for item in current_navbar_items:
-		navbar_settings.append(
-			"settings_dropdown",
-			{
-				"item_label": item.item_label,
-				"item_type": item.item_type,
-				"route": item.route,
-				"action": item.action,
-				"is_standard": item.is_standard,
-				"hidden": item.hidden,
-			},
-		)
-
-	navbar_settings.save()
