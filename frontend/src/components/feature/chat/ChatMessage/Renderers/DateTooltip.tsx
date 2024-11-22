bab122610be48ea7b6c48e024a0393848cff965f
Nikhil Kothari <nik.kothari22@live.com>
2024-11-22 18:25:33 +0530

    perf: memoize DateTooltip

--- frontend/src/components/feature/chat/ChatMessage/Renderers/DateTooltip.tsx
+++ frontend/src/components/feature/chat/ChatMessage/Renderers/DateTooltip.tsx
@@ -1,17 +1,29 @@
-import { DateMonthAtHourMinuteAmPm, HourMinuteAmPm } from '@/utils/dateConversions'
 import { useMemo } from 'react'
 import { Tooltip, Link } from '@radix-ui/themes'
+import { getDateObject } from '@/utils/dateConversions/utils'
+
 export const DateTooltip = ({ timestamp }: { timestamp: string }) => {
-    const memoizedTime = useMemo(() => <HourMinuteAmPm date={timestamp} />, [timestamp])
+
+    const { tooltipContent, time } = useMemo(() => {
+
+        const dateObj = getDateObject(timestamp)
+
+        return {
+            tooltipContent: dateObj.format("Do MMMM [at] hh:mm A"),
+            time: dateObj.format("hh:mm A")
+        }
+
+    }, [timestamp])
+
     return (
-        <Tooltip content={<DateMonthAtHourMinuteAmPm date={timestamp} />}>
+        <Tooltip content={tooltipContent}>
             <Link
                 asChild
                 size='1'
                 color="gray"
             >
                 <span>
-                    {memoizedTime}
+                    {time}
                 </span>
             </Link>
         </Tooltip>
@@ -19,23 +31,26 @@ export const DateTooltip = ({ timestamp }: { timestamp: string }) => {
 }
 
 export const DateTooltipShort = ({ timestamp }: { timestamp: string }) => {
-    const memoizedContent = useMemo(
-        () => <DateMonthAtHourMinuteAmPm date={timestamp} />,
-        [timestamp]
-    )
-    const memoizedTime = useMemo(
-        () => <HourMinuteAmPm date={timestamp} amPm={false} />,
-        [timestamp]
-    )
+
+    const { tooltipContent, time } = useMemo(() => {
+
+        const dateObj = getDateObject(timestamp)
+
+        return {
+            tooltipContent: dateObj.format("Do MMMM [at] hh:mm A"),
+            time: dateObj.format("hh:mm")
+        }
+
+    }, [timestamp])
 
     return (
-        <Tooltip content={memoizedContent}>
+        <Tooltip content={tooltipContent}>
             <Link
                 asChild
                 style={{ fontSize: '0.68rem' }}
                 color="gray"
             >
-                <span>{memoizedTime}</span>
+                <span>{time}</span>
             </Link>
         </Tooltip>
     )
