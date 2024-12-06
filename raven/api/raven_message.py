2f80f9a1feb631f033f767713eb061056553222d
Nikhil Kothari <nik.kothari22@live.com>
2024-12-06 20:35:48 +0530

    fix: only fetch unread counts for members

--- raven/api/raven_message.py
+++ raven/api/raven_message.py
@@ -217,7 +217,7 @@ def get_unread_count_for_channels():
 		.on(
 			(channel.name == channel_member.channel_id) & (channel_member.user_id == frappe.session.user)
 		)
-		.where((channel.type == "Open") | (channel_member.user_id == frappe.session.user))
+		.where(channel_member.user_id == frappe.session.user)
 		.where(channel.is_archived == 0)
 		.where(channel.is_thread == 0)
 		.where(message.message_type != "System")
