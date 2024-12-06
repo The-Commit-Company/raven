a0287fe69b9daaf115ffb33e08c184ad48397ed7
Nikhil Kothari <nik.kothari22@live.com>
2024-12-06 20:17:22 +0530

    fix: route using effect

--- frontend/src/pages/WorkspaceSwitcher.tsx
+++ frontend/src/pages/WorkspaceSwitcher.tsx
@@ -2,7 +2,8 @@ import { ErrorBanner } from '@/components/layout/AlertBanner/ErrorBanner'
 import { Stack } from '@/components/layout/Stack'
 import useFetchWorkspaces from '@/hooks/fetchers/useFetchWorkspaces'
 import { Flex, Text } from '@radix-ui/themes'
-import { Navigate, Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 const WorkspaceSwitcher = () => {
 
@@ -11,7 +12,18 @@ const WorkspaceSwitcher = () => {
     const lastWorkspace = localStorage.getItem('ravenLastWorkspace') ?? ''
     const lastChannel = localStorage.getItem('ravenLastChannel') ?? ''
 
-    const isWorkspaceInData = data?.message.some(w => w.name === lastWorkspace) ?? false
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const isWorkspaceInData = data?.message.some(w => w.name === lastWorkspace) ?? false
+        if (lastWorkspace && isWorkspaceInData && lastChannel) {
+            navigate(`/${lastWorkspace}/${lastChannel}`, { replace: true })
+        } else {
+            if (data?.message.length === 0) {
+                navigate(`/${data.message[0].name}`, { replace: true })
+            }
+        }
+    }, [data])
 
     if (isLoading) {
         return <Flex justify='center' align='center' height='100vh' width='100vw' className='animate-fadein'>
@@ -26,10 +38,6 @@ const WorkspaceSwitcher = () => {
         return <Flex justify='center' align='center' height='100vh' width='100vw'>
             <ErrorBanner error={error} overrideHeading='There was an error while fetching your workspaces.' />
         </Flex>
-    }
-
-    if (data && lastWorkspace && lastChannel && isWorkspaceInData) {
-        return <Navigate to={`/${lastWorkspace}/${lastChannel}`} replace />
     }
 
     if (data && data?.message.length === 0) {
