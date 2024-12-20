92dd1c7f678fa36b06e5d70dc3b7bbb397068a20
Janhvi Patil <janhvipatil716@gmail.com>
2024-12-20 21:10:53 +0530

    chore: create channel button

--- apps/mobile/components/features/chat/CreateChannel/CreateChannel.tsx
+++ apps/mobile/components/features/chat/CreateChannel/CreateChannel.tsx
@@ -22,7 +22,7 @@ const CreateChannel = () => {
             <Pressable style={styles.addChannelButton}
                 onPress={handleCreateChannel}
                 // Use tailwind classes for layout and ios:active state
-                className={`flex-row items-center px-3 py-2 rounded-lg ios:active:bg-[${colors.colors.linkColor}] ios:active:dark:bg-[${colors.colors.linkColor}]`}
+                className={`flex-row items-center px-3 py-2 rounded-lg ios:active:bg-linkColor`}
                 // Add a subtle ripple effect on Android
                 android_ripple={{ color: 'rgba(0,0,0,0.1)', borderless: false }}>
                 <PlusIcon fill={colors.colors.icon} height={18} width={18} />
