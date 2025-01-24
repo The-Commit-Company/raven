7c30670c25c47720384300688bc01589f434219b
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2025-01-24 17:14:28 +0530

    feat: implement MessageContentRenderer to display message text and link previews

---
+++ apps/mobile/components/features/chat-stream/MessageItemElements/MessageContentRenderer.tsx
@@ -0,0 +1,44 @@
+import { Message } from "@raven/types/common/Message"
+import LinkPreview from "./LinkPreview"
+import MessageTextRenderer from "@components/features/chat-stream/MessageItemElements/MessageTextRenderer"
+import * as htmlparser2 from 'htmlparser2';
+
+type MessageContentRendererProps = {
+    message: Message,
+    showLinkPreview: boolean
+}
+
+export const MessageContentRenderer = ({ message, showLinkPreview }: MessageContentRendererProps) => {
+
+    const firstLink = extractFirstLink(message.text)
+
+    if (showLinkPreview && firstLink) {
+        return <LinkPreview messageID={message.name} href={firstLink} />
+    }
+    return (
+        <MessageTextRenderer text={message.text} />
+    )
+}
+
+// in message search for first link and extract href to show link preview
+const extractFirstLink = (html: string): string | null => {
+    let firstLink: string | null = null
+
+    const parser = new htmlparser2.Parser({
+        onopentag(name, attributes) {
+            if (!firstLink && name === 'a' && attributes.href) {
+                // Ignore mailto: links and ensure it's an http(s) link
+                if (!attributes.href.startsWith('mailto:') &&
+                    (attributes.href.startsWith('http://') ||
+                        attributes.href.startsWith('https://'))) {
+                    firstLink = attributes.href
+                }
+            }
+        }
+    })
+
+    parser.write(html)
+    parser.end()
+
+    return firstLink
+}
