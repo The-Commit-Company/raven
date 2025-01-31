40b54a34bc0049db369d5af6589fa3e845b1cb3c
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2025-01-31 13:33:39 +0530

    feat: improve chat header navigation and settings access on header title press

--- apps/mobile/app/[site_id]/chat/[id]/index.tsx
+++ apps/mobile/app/[site_id]/chat/[id]/index.tsx
@@ -1,21 +1,36 @@
-
-
-import { TextInput, View } from 'react-native'
-import { Stack, useLocalSearchParams } from 'expo-router';
+import { TextInput, TouchableOpacity, View } from 'react-native'
+import { router, Stack, useLocalSearchParams } from 'expo-router';
 import { Button } from '@components/nativewindui/Button';
 import { Text } from '@components/nativewindui/Text';
 import ChatStream from '@components/features/chat-stream/ChatStream';
-import { ChannelSettingsButton } from '@components/features/channel-settings/ChannelSettingsButton';
 
 const Chat = () => {
     const { id } = useLocalSearchParams();
+
+    const handleOnTitlePress = () => {
+        router.push('./channel-settings', {
+            relativeToDirectory: true
+        })
+    }
 
     return (
         <>
             <Stack.Screen options={{
                 headerBackButtonDisplayMode: 'minimal',
-                title: id as string,
-                headerRight: () => <ChannelSettingsButton />
+                headerRight: undefined,
+                headerTitle: () => {
+                    return (
+                        <TouchableOpacity
+                            onPress={handleOnTitlePress}
+                            className='flex-1'
+                            activeOpacity={0.5}
+                        >
+                            <View className='flex-row justify-center items-center'>
+                                <Text className='text-md font-bold'>{id as string}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    )
+                }
             }} />
             <View className='flex-1'>
                 <ChatStream channelID={id as string} />
