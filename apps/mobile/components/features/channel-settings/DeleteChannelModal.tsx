067a6112520bc97cf71b1c541b750755d7e491d9
Janhvi Patil <janhvipatil716@gmail.com>
2025-02-09 17:41:17 +0530

    refactor(mobile): updated delete channel modal

--- apps/mobile/components/features/channel-settings/DeleteChannelModal.tsx
+++ apps/mobile/components/features/channel-settings/DeleteChannelModal.tsx
@@ -1,117 +1,96 @@
 import { Sheet } from "@components/nativewindui/Sheet";
 import { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
-import { View } from "react-native";
+import { View, Pressable } from "react-native";
 import { Button } from "@components/nativewindui/Button";
 import { Text } from "@components/nativewindui/Text";
 import { ChannelListItem } from "@raven/types/common/ChannelListItem";
-import { Checkbox } from "@components/nativewindui/Checkbox";
 import { useContext, useState } from "react";
 import { useFrappeDeleteDoc } from "frappe-react-sdk";
 import { router } from "expo-router";
 import { SiteContext } from "app/[site_id]/_layout";
 import { toast } from "sonner-native";
+import CheckIcon from "@assets/icons/CheckIcon.svg";
+import { useColorScheme } from '@hooks/useColorScheme';
+import { ChannelListContext, ChannelListContextType } from "@raven/lib/providers/ChannelListProvider";
 
 interface DeleteChannelModalProps {
-    deleteSheetRef: React.RefObject<BottomSheetModal>;
+    deleteSheetRef: React.RefObject<BottomSheetModal>
     channelData: ChannelListItem
 }
 
-export const DeleteChannelModal: React.FC<DeleteChannelModalProps> = ({
-    deleteSheetRef,
-    channelData
-}) => {
+export const DeleteChannelModal: React.FC<DeleteChannelModalProps> = ({ deleteSheetRef, channelData }) => {
 
     const { deleteDoc, error, loading: deletingDoc } = useFrappeDeleteDoc()
+    const { mutate } = useContext(ChannelListContext) as ChannelListContextType
 
     const [allowDelete, setAllowDelete] = useState(false)
 
     const siteContext = useContext(SiteContext)
     const siteId = siteContext?.sitename
+    const { colors } = useColorScheme()
 
     const handleDeleteChannel = async () => {
         if (channelData?.name) {
             deleteDoc('Raven Channel', channelData.name)
                 .then(() => {
-                    // mutate(["channel_members", channelData.name], undefined, { revalidate: false })
+                    toast.success(`Channel ${channelData?.channel_name} has been deleted.`)
+                    mutate()
                     handleClose()
                     router.replace(`/${siteId}/home`)
-                    // if (lastWorkspace) {
-                    //     navigate(`/${lastWorkspace}`)
-                    // } else {
-                    //     navigate('/')
-                    // }
-
-                    toast.success(`Channel ${channelData.channel_name} deleted.`)
+                })
+                .catch(() => {
+                    toast.error('Could not delete channel', {
+                        description: error?.httpStatusText
+                    })
                 })
         }
     };
 
     const handleClose = () => {
-        deleteSheetRef.current?.dismiss();
-    };
+        deleteSheetRef.current?.dismiss()
+    }
+
+    const toggleAllowDelete = () => {
+        setAllowDelete(!allowDelete)
+    }
 
     return (
-        <Sheet ref={deleteSheetRef} snapPoints={[500]} >
+        <Sheet ref={deleteSheetRef}>
             {props => {
                 return (
-                    <BottomSheetView
-                        {...props}
-                        className="gap-3"
-                    >
-                        <View className="flex-col gap-3 justify-start px-5 pt-5 pb-6">
-                            <Text className="text-xl font-cal-sans">
-                                Delete this Channel?
+                    <BottomSheetView {...props}>
+                        <View className="flex-col px-4 gap-3 mt-2 mb-16">
+                            <Text className="text-xl font-cal-sans">Delete this Channel?</Text>
+                            <Text className="text-sm">
+                                Please understand that when you delete <Text className="text-sm font-semibold">{channelData?.channel_name}</Text>:
                             </Text>
-                            <Text
-                                className="text-sm"
-                            >
-                                Please understand that when you delete {channelData?.channel_name}:
+                            <Text className="text-sm">
+                                All messages, including files and images will be removed
                             </Text>
-                            <View
-                                className="gap-2 list-inside"
-                            >
-                                <Text
-                                    className="text-sm"
-                                >
-                                    All messages, including files and images will be removed
-                                </Text>
-                                <Text
-                                    className="text-sm"
-                                >
-                                    You can archive this channel instead to preserve your messages
-                                </Text>
-                            </View>
-                            <View className="flex-row gap-2 align-center">
-                                <Checkbox
-                                    onCheckedChange={() => setAllowDelete(!allowDelete)}
-                                    checked={allowDelete}
-                                />
-                                <Text className="text-base text-gray-800 dark:text-gray-200" >
+                            <Text className="text-sm text-muted-foreground">
+                                (You can archive this channel instead to preserve your messages)
+                            </Text>
+                            <Pressable onPress={toggleAllowDelete} className="flex-row gap-2 items-center pt-3">
+                                {allowDelete ?
+                                    <View className="border border-border rounded-full p-0.5">
+                                        <CheckIcon fill={colors.icon} height={20} width={20} />
+                                    </View>
+                                    : <View className="border border-border rounded-full p-3"></View>
+                                }
+                                <Text className="text-sm">
                                     Yes, I understand, permanently delete this channel
                                 </Text>
-                            </View>
-                        </View>
-                        <View className="gap-3 justify-start px-5">
-                            <Button
-                                onPress={handleDeleteChannel}
-                                disabled={!allowDelete || deletingDoc}
-                            >
-                                <Text>
-                                    Delete Channel
-                                </Text>
+                            </Pressable>
+                            <Button onPress={handleDeleteChannel} disabled={!allowDelete || deletingDoc} className="bg-red-600">
+                                <Text>{deletingDoc ? 'Deleting...' : 'Delete Channel'}</Text>
                             </Button>
-                            <Button
-                                onPress={handleClose}
-                                variant="secondary"
-                            >
-                                <Text>
-                                    Cancel
-                                </Text>
+                            <Button onPress={handleClose} variant="plain" className="border border-border">
+                                <Text>Cancel</Text>
                             </Button>
                         </View>
                     </BottomSheetView>
                 )
             }}
         </Sheet>
-    );
+    )
 }
