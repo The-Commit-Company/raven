cb76745858fbeb8a46103b3d98601686ab192ac7
Janhvi Patil <janhvipatil716@gmail.com>
2025-02-10 12:00:43 +0530

    feat(mobile): first message block for DM channels

--- apps/mobile/components/features/chat-stream/ChatStream.tsx
+++ apps/mobile/components/features/chat-stream/ChatStream.tsx
@@ -9,6 +9,7 @@ import MessageItem from './MessageItem'
 import { PollMessageBlock } from '../chat/ChatMessage/Renderers/PollMessage'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useKeyboardVisible } from '@hooks/useKeyboardVisible'
+import ChannelHistoryFirstMessage from './FirstMessageBlock'
 
 type Props = {
     channelID: string
@@ -60,6 +61,7 @@ const ChatStream = ({ channelID }: Props) => {
                     getEstimatedItemSize={getEstimatedItemSize}
                     renderItem={MessageContentRenderer}
                     recycleItems={false}
+                    ListHeaderComponent={<ChannelHistoryFirstMessage channelID={channelID} />}
                 />
             </View>
         )
