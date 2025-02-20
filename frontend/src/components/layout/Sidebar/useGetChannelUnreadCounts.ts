25f89b80984992671a55e5756df2cfcdffeaeb8d
Nikhil Kothari <nik.kothari22@live.com>
2025-02-20 22:11:22 +0530

    feat: add toggle in sidebar for showing/hiding all channels

--- frontend/src/components/layout/Sidebar/useGetChannelUnreadCounts.ts
+++ frontend/src/components/layout/Sidebar/useGetChannelUnreadCounts.ts
@@ -1,5 +1,7 @@
 import { ChannelListItem, DMChannelListItem, UnreadCountData } from '@/utils/channel/ChannelListProvider';
+import { useAtomValue } from 'jotai';
 import { useMemo } from 'react';
+import { showOnlyMyChannelsAtom } from './SidebarBody';
 
 export interface UseGetChannelUnreadCountProps {
     channels: ChannelListItem[]
@@ -16,6 +18,8 @@ export interface DMChannelWithUnreadCount extends DMChannelListItem {
 }
 
 export const useGetChannelUnreadCounts = ({ channels, dm_channels, unread_count }: UseGetChannelUnreadCountProps) => {
+
+    const showOnlyMyChannels = useAtomValue(showOnlyMyChannelsAtom)
 
     const { unreadChannels, readChannels, unreadDMs, readDMs } = useMemo(() => {
 
@@ -34,36 +38,53 @@ export const useGetChannelUnreadCounts = ({ channels, dm_channels, unread_count 
         const allChannels: (ChannelListItem | DMChannelListItem)[] = [...channels, ...dm_channels]
 
         // Process all channels and DMs to separate unread and read
-        allChannels.filter((channel) => {
+        allChannels.forEach(channel => {
+
             if (channel.is_archived) {
-                return false
+                // If the channel is archived, do not add to array
+                return
             }
-            if (channel.type === "Public" && !channel.member_id) {
-                return false
-            }
-            return true
-        }).forEach(channel => {
             const unreadCount = unreadCounts[channel.name] || 0
             const channelWithUnread = { ...channel, unread_count: unreadCount }
 
             if (unreadCount > 0) {
+                // If the channel has unread messages
                 if ('is_direct_message' in channel && channel.is_direct_message) {
+                    // If it is a DM channel, add to unreadDMs
                     unreadDMs.push(channelWithUnread as DMChannelWithUnreadCount)
                 } else {
-                    unreadChannels.push(channelWithUnread as ChannelWithUnreadCount)
+                    // Else it's a regular channel
+                    // For channels that are public but the user is not a member, do not add to unread channel array
+                    if (channel.type === "Public" && !channel.member_id) {
+                        if (!showOnlyMyChannels) {
+                            // Instead add to readChannels if showOnlyMyChannels is false
+                            readChannels.push(channelWithUnread as ChannelWithUnreadCount)
+                        }
+                    } else {
+                        unreadChannels.push(channelWithUnread as ChannelWithUnreadCount)
+                    }
+
                 }
             } else {
                 if ('is_direct_message' in channel && channel.is_direct_message) {
                     readDMs.push(channelWithUnread as DMChannelWithUnreadCount)
                 } else {
-                    readChannels.push(channelWithUnread as ChannelWithUnreadCount)
+                    console.log(channel.member_id, showOnlyMyChannels)
+                    let shouldAddToReadChannels = true
+
+                    if (showOnlyMyChannels && !channel.member_id) {
+                        shouldAddToReadChannels = false
+                    }
+                    if (shouldAddToReadChannels) {
+                        readChannels.push(channelWithUnread as ChannelWithUnreadCount)
+                    }
                 }
             }
         });
 
         return { unreadChannels, readChannels, unreadDMs, readDMs }
 
-    }, [channels, dm_channels, unread_count])
+    }, [channels, dm_channels, unread_count, showOnlyMyChannels])
 
     return { unreadChannels, readChannels, unreadDMs, readDMs }
 }
