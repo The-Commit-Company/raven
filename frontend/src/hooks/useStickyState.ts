fbf3e64993e282c26017a4bb530bdfd12fbfee05
Nikhil Kothari <nik.kothari22@live.com>
2025-02-20 19:01:18 +0530

    feat: store draft messages in local storage

--- frontend/src/hooks/useStickyState.ts
+++ frontend/src/hooks/useStickyState.ts
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 
-export function useStickyState<T = any>(defaultValue: T, key: string): [T, React.Dispatch<React.SetStateAction<T>>] {
+export function useStickyState<T = any>(defaultValue: T, key: string, overrideLocalStorage: boolean = false): [T, React.Dispatch<React.SetStateAction<T>>] {
     const [value, setValue] = useState<T>(() => {
-        const stickyValue = window.localStorage.getItem(key);
-        return stickyValue !== null
-            ? JSON.parse(stickyValue)
-            : defaultValue;
+        if (overrideLocalStorage) {
+            return defaultValue;
+        } else {
+            const stickyValue = window.localStorage.getItem(key);
+            return stickyValue !== null
+                ? JSON.parse(stickyValue)
+                : defaultValue;
+        }
     });
     useEffect(() => {
         window.localStorage.setItem(key, JSON.stringify(value));
