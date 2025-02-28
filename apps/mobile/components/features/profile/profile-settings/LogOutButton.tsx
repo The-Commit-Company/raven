cc51a4ee0c5525f40ff49ea58592805ca1145cf7
Janhvi Patil <janhvipatil716@gmail.com>
2025-02-28 18:25:07 +0530

    fix(mobile): log out button interaction in profile page

--- apps/mobile/components/features/profile/profile-settings/LogOutButton.tsx
+++ apps/mobile/components/features/profile/profile-settings/LogOutButton.tsx
@@ -37,7 +37,7 @@ const LogOutButton = () => {
 
     return (
         <Pressable onPress={onLogout}
-            className="flex flex-row items-center py-3 px-4 rounded-xl justify-between bg-background dark:bg-card ios:active:bg-red-50">
+            className="flex flex-row items-center py-3 px-4 rounded-xl justify-between bg-background dark:bg-card ios:active:bg-red-50 dark:ios:active:bg-red-100/10">
             <Text className="font-medium text-destructive">Log Out</Text>
             <LogOutIcon height={16} width={16} color={colors.grey} />
         </Pressable>
