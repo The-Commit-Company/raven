26724131f7ea3c178258e95d200a79d4db260708
Janhvi Patil <janhvipatil716@gmail.com>
2025-03-07 15:30:16 +0530

    feat(mobile): added thread list component and hook for unread threads count

--- apps/mobile/components/features/threads/AIThreads.tsx
+++ apps/mobile/components/features/threads/AIThreads.tsx
@@ -1,9 +1,40 @@
 import { View } from "react-native"
-import { Text } from "@components/nativewindui/Text"
+import { useState } from "react"
+import { useDebounce } from "@raven/lib/hooks/useDebounce"
+import { SearchInput } from "@components/nativewindui/SearchInput"
+import { useColorScheme } from "@hooks/useColorScheme"
+import ThreadsList from "./ThreadsList"
+
 const AIThreads = () => {
+
+    const [onlyShowUnread, setOnlyShowUnread] = useState(false)
+    const [searchQuery, setSearchQuery] = useState("")
+    const debouncedText = useDebounce(searchQuery, 200)
+    const { colors } = useColorScheme()
+
     return (
         <View>
-            <Text>AIThreads</Text>
+            <View className='flex gap-2 justify-between p-2 border-b border-gray-4'>
+                <View className='px-4 py-3'>
+                    <SearchInput
+                        style={{ backgroundColor: colors.grey6 }}
+                        placeholder="Search"
+                        placeholderTextColor={colors.grey}
+                        onChangeText={setSearchQuery}
+                        value={searchQuery}
+                    />
+                </View>
+                {/* <View className='flex gap-2'>
+                    <UnreadFilter onlyShowUnread={onlyShowUnread} setOnlyShowUnread={setOnlyShowUnread} />
+                </View> */}
+            </View>
+            <View className="h-[calc(100vh-10rem)] overflow-y-auto">
+                <ThreadsList
+                    content={debouncedText}
+                    aiThreads={1}
+                    onlyShowUnread={onlyShowUnread}
+                />
+            </View>
         </View>
     )
 }
--- apps/mobile/components/features/threads/ThreadTabs.tsx
+++ apps/mobile/components/features/threads/ThreadTabs.tsx
@@ -5,10 +5,34 @@ import OtherThreads from './OtherThreads'
 import ParticipatingThreads from './ParticipatingThreads'
 import { SegmentedControl } from '@components/nativewindui/SegmentedControl/SegmentedControl'
 
-const ThreadTabs = ({ threadID }: { threadID: string }) => {
+export type ThreadMessage = {
+    bot: string,
+    channel_id: string,
+    content: string,
+    creation: string,
+    file: string,
+    hide_link_preview: 0 | 1,
+    image_height: string
+    image_width: string,
+    is_bot_message: 0 | 1,
+    last_message_timestamp: string,
+    link_doctype: string,
+    link_document: string,
+    message_type: "Text" | "Image" | "File" | "Poll",
+    name: string,
+    owner: string,
+    poll_id: string,
+    text: string,
+    thread_message_id: string,
+    participants: { user_id: string }[],
+    workspace?: string,
+    reply_count?: number
+}
+
+const ThreadTabs = () => {
 
     const [selectedIndex, setSelectedIndex] = useState(0)
-    const values = ['Participating', 'Other', 'AI Threads']
+    const values = ['Participating', 'Other', 'AI Agents']
 
     const handleIndexChange = (index: number) => {
         setSelectedIndex(index)
