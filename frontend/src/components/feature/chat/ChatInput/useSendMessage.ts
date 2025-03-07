f6d4183e17c579982e9bb75e838fe7e60478ebc4
Nikhil Kothari <nik.kothari22@live.com>
2025-03-07 16:59:34 +0530

    Update frontend/src/components/feature/chat/ChatInput/useSendMessage.ts

--- frontend/src/components/feature/chat/ChatInput/useSendMessage.ts
+++ frontend/src/components/feature/chat/ChatInput/useSendMessage.ts
@@ -1,6 +1,6 @@
 import { useFrappePostCall } from 'frappe-react-sdk'
 import { Message } from '../../../../../../types/Messaging/Message'
-import { RavenMessage } from '@raven/types/RavenMessaging/RavenMessage'
+import { RavenMessage } from '@/types/RavenMessaging/RavenMessage'
 
 export const useSendMessage = (channelID: string, noOfFiles: number, uploadFiles: () => Promise<RavenMessage[]>, onMessageSent: (messages: RavenMessage[]) => void, selectedMessage?: Message | null) => {
 
