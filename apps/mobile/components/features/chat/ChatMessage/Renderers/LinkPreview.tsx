dd52f5f68204e48391d5636f00c11fd740314651
Nikhil Kothari <nik.kothari22@live.com>
2025-03-08 19:10:21 +0530

    fix(mobile): link preview UI

--- apps/mobile/components/features/chat/ChatMessage/Renderers/LinkPreview.tsx
+++ apps/mobile/components/features/chat/ChatMessage/Renderers/LinkPreview.tsx
@@ -1,7 +1,6 @@
-import { useHideLinkPreview, useLinkPreview } from '@hooks/useLinkPreview';
+import { useLinkPreview } from '@hooks/useLinkPreview';
 import { memo } from 'react';
 import { View, Text, TouchableOpacity, Linking, Image } from 'react-native';
-import CrossIcon from "@assets/icons/CrossIcon.svg"
 
 type LinkPreviewProps = {
     messageID: string;
@@ -11,7 +10,6 @@ type LinkPreviewProps = {
 const LinkPreview = memo(({ messageID, href }: LinkPreviewProps) => {
 
     const linkPreview = useLinkPreview(href);
-    const hideLinkPreview = useHideLinkPreview(messageID);
 
     const handleLinkPress = async () => {
         if (href) {
@@ -27,38 +25,28 @@ const LinkPreview = memo(({ messageID, href }: LinkPreviewProps) => {
     const image = linkPreview.absolute_image || linkPreview.image;
 
     return (
-        <View className="w-[80%] border border-gray-200 dark:border-gray-700 bg-gray-100 dark:bg-gray-900 rounded-lg overflow-hidden relative">
+        <View className="w-full border border-border bg-card-background/40 rounded-lg overflow-hidden relative">
             <TouchableOpacity onPress={handleLinkPress} activeOpacity={0.7}>
                 <View>
                     <Image
                         source={{ uri: image }}
-                        className="w-full h-48 bg-gray-100"
+                        className="w-full h-36 bg-card-background/20"
                     />
                     <View className="p-2 pt-2.5">
                         <View className='gap-1'>
-                            <Text className="text-md dark:text-white font-bold" numberOfLines={1}>
+                            <Text className="text-md text-foreground font-bold" numberOfLines={1}>
                                 {linkPreview.title}
                             </Text>
-                            <Text className="text-sm text-gray-500 dark:text-gray-400">
+                            <Text className="text-sm text-muted-foreground">
                                 {linkPreview.site_name}
                             </Text>
                         </View>
-                        <Text className="text-xs text-gray-500 pt-1.5" numberOfLines={10}>
+                        <Text className="text-xs text-muted-foreground pt-1.5" numberOfLines={10}>
                             {linkPreview.description?.replace(/\n/g, ' ')}
                         </Text>
                     </View>
                 </View>
             </TouchableOpacity>
-
-            {!!linkPreview ? (
-                <TouchableOpacity
-                    onPress={hideLinkPreview}
-                    className="absolute top-2 right-2 w-7 h-7 rounded-full bg-gray-700 dark:bg-gray-800 items-center justify-center"
-                    activeOpacity={0.7}
-                >
-                    <CrossIcon width={18} height={18} fill="lightgray" />
-                </TouchableOpacity>
-            ) : null}
         </View>
     );
 });
