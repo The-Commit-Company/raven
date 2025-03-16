55e275a829a029cd4f3ea86a5467361cf3abf7fa
Nikhil Kothari <nik.kothari22@live.com>
2025-03-16 17:50:21 +0530

    fix(mobile): set default theme to system

--- apps/mobile/app/landing.tsx
+++ apps/mobile/app/landing.tsx
@@ -3,14 +3,13 @@ import { View } from 'react-native';
 import { Text } from '@components/nativewindui/Text';
 import AddSite from '@components/features/auth/AddSite';
 
-
 export default function LandingScreen() {
 
     return (
         <>
             <Stack.Screen options={{ title: 'Sites', headerShown: false }} />
             <View className='flex-1 justify-center pt-48 px-6 gap-3'>
-                <Text className='text-5xl font-cal-sans'>raven</Text>
+                <Text className='text-5xl font-cal-sans text-foreground'>raven</Text>
                 <View className='h-2' />
                 <AddSite />
             </View>
