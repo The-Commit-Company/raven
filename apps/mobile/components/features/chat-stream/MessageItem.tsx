0f994385efefb1779812d3992d3e5b66cb547db4
Nikhil Kothari <nik.kothari22@live.com>
2025-03-21 15:56:23 +0530

    fix(mobile): removed conflict with long press gesture to view reactions

--- apps/mobile/components/features/chat-stream/MessageItem.tsx
+++ apps/mobile/components/features/chat-stream/MessageItem.tsx
@@ -131,7 +131,7 @@ const MessageItem = memo(({ message }: Props) => {
 
                             {message.is_edited === 1 && <Text className='text-xs text-muted-foreground'>(edited)</Text>}
                             {message.hide_link_preview === 0 && message.text && <MessageLinkRenderer message={message} />}
-                            <MessageReactions message={message} />
+                            <MessageReactions message={message} longPressGesture={longPressGesture} />
                         </View>
                     </View>
                 </View>
