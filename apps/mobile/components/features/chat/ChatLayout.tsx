ba355ca11993b56eef787c680ae18f15309f62a3
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2025-03-28 14:14:06 +0530

    feat(mobile): add JoinChannelBox functionality

--- apps/mobile/components/features/chat/ChatLayout.tsx
+++ apps/mobile/components/features/chat/ChatLayout.tsx
@@ -1,6 +1,6 @@
 import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 import { useKeyboardHandler } from 'react-native-keyboard-controller';
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { LegendListRef } from '@legendapp/list';
 import MessageActionsBottomSheet from '@components/features/chat/ChatMessage/MessageActions/MessageActionsBottomSheet';
 import { useSheetRef } from '@components/nativewindui/Sheet';
@@ -9,6 +9,10 @@ import { messageActionsSelectedMessageAtom } from '@lib/ChatInputUtils';
 import { Platform, View } from 'react-native';
 import ChatStream from '../chat-stream/ChatStream';
 import ChatInput from './ChatInput/ChatInput';
+import { useCurrentChannelData } from '@hooks/useCurrentChannelData';
+import { Member, useFetchChannelMembers } from '@raven/lib/hooks/useFetchChannelMembers';
+import useCurrentRavenUser from '@raven/lib/hooks/useCurrentRavenUser';
+import { JoinChannelBox } from './ChatFooter/JoinChannelBox';
 
 const PADDING_BOTTOM = Platform.OS === 'ios' ? 20 : 0;
 
@@ -31,6 +35,13 @@ type Props = {
 const ChatLayout = ({ channelID, isThread = false }: Props) => {
 
     const { height } = useGradualAnimation()
+
+    const { channel } = useCurrentChannelData(channelID)
+    const channelData = channel?.channelData
+    const type = channel?.type
+
+    const { myProfile } = useCurrentRavenUser()
+    const { channelMembers, isLoading } = useFetchChannelMembers(channelID)
 
     const fakeView = useAnimatedStyle(() => {
         return {
@@ -59,6 +70,50 @@ const ChatLayout = ({ channelID, isThread = false }: Props) => {
         setSelectedMessage(null)
     }
 
+    const channelMemberProfile: Member | null = useMemo(() => {
+        if (myProfile?.name && channelMembers) {
+            return channelMembers[myProfile?.name] ?? null
+        }
+        return null
+    }, [myProfile?.name, channelMembers])
+
+    const { canUserSendMessage, shouldShowJoinBox } = useMemo(() => {
+
+        if (channelData?.is_archived) {
+            return {
+                canUserSendMessage: false,
+                shouldShowJoinBox: false
+            }
+        }
+
+        if (channelData?.type === 'Open') {
+            return {
+                canUserSendMessage: true,
+                shouldShowJoinBox: false
+            }
+        }
+
+        if (channelMemberProfile) {
+            return {
+                canUserSendMessage: true,
+                shouldShowJoinBox: false
+            }
+        }
+
+        const isDM = channelData?.is_direct_message === 1 || channelData?.is_self_message === 1
+
+        // If the channel data is loaded and the member profile is loaded, then check for this, else don't show anything.
+        if (!channelMemberProfile && !isDM && channelData && !isLoading) {
+            return {
+                shouldShowJoinBox: true,
+                canUserSendMessage: false
+            }
+        }
+
+        return { canUserSendMessage: false, shouldShowJoinBox: false }
+
+    }, [channelMemberProfile, channelData, isLoading])
+
     useEffect(() => {
         return () => {
             setSelectedMessage(null)
@@ -76,10 +131,20 @@ const ChatLayout = ({ channelID, isThread = false }: Props) => {
         <>
             <View className='flex-1'>
                 <ChatStream channelID={channelID} scrollRef={scrollRef} isThread={isThread} />
-                <ChatInput channelID={channelID} onSendMessage={onSendMessage} />
-                <Animated.View style={fakeView} />
+                {
+                    canUserSendMessage &&
+                    <>
+                        <ChatInput channelID={channelID} onSendMessage={onSendMessage} />
+                        <Animated.View style={fakeView} />
+                    </>
+                }
             </View>
-
+            {
+                shouldShowJoinBox &&
+                <JoinChannelBox
+                    channelData={channelData}
+                    user={myProfile?.name ?? ""} />
+            }
             <MessageActionsBottomSheet
                 messageActionsSheetRef={messageActionsSheetRef}
                 message={selectedMessage}
