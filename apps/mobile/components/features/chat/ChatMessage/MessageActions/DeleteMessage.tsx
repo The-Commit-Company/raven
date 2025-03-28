f4c9500de769aff63671a60a768b69d33b12c71e
Nikhil Kothari <nik.kothari22@live.com>
2025-03-28 19:35:04 +0530

    feat(mobile): optimistically delete message in chat stream

--- apps/mobile/components/features/chat/ChatMessage/MessageActions/DeleteMessage.tsx
+++ apps/mobile/components/features/chat/ChatMessage/MessageActions/DeleteMessage.tsx
@@ -1,13 +1,14 @@
 import { Alert } from '@components/nativewindui/Alert'
 import { AlertRef } from '@components/nativewindui/Alert/types'
 import { Message } from '@raven/types/common/Message'
-import { useFrappeDeleteDoc } from 'frappe-react-sdk'
+import { useFrappeDeleteDoc, useSWRConfig } from 'frappe-react-sdk'
 import React, { useCallback } from 'react'
 import { Pressable, View } from 'react-native'
 import { toast } from 'sonner-native'
 import { Text } from '@components/nativewindui/Text'
 import TrashIcon from "@assets/icons/TrashIcon.svg"
 import { useColorScheme } from '@hooks/useColorScheme'
+import { GetMessagesResponse } from '@raven/types/common/ChatStream'
 
 interface DeleteMessageProps {
     message: Message
@@ -62,16 +63,44 @@ export default DeleteMessage
 
 const useMessageDelete = (message: Message) => {
 
+    const { mutate } = useSWRConfig()
+
     const { deleteDoc, loading } = useFrappeDeleteDoc()
 
     const deleteMessage = (onSuccess: () => void) => {
-        deleteDoc('Raven Message', message.name).then(() => {
-            toast.success('Message deleted', {
-                duration: 500,
+
+        const removeMessageFromCache = (data?: GetMessagesResponse): GetMessagesResponse => {
+
+            const existingMessages = data?.message.messages ?? []
+
+            const newMessages = existingMessages.filter((m) => m.name !== message.name)
+
+            return {
+                message: {
+                    has_old_messages: data?.message.has_old_messages ?? true,
+                    has_new_messages: data?.message.has_new_messages ?? false,
+                    messages: newMessages
+                }
+            }
+        }
+
+        // Delete the message optimistically
+        mutate({ path: `get_messages_for_channel_${message.channel_id}` }, async (data?: GetMessagesResponse) => {
+            // Make the request
+            return deleteDoc('Raven Message', message.name).then(() => {
+                toast.success('Message deleted', {
+                    duration: 500,
+                })
+                onSuccess()
+            }).then(() => {
+                return removeMessageFromCache(data)
             })
-            onSuccess()
+        }, {
+            optimisticData: removeMessageFromCache,
+            rollbackOnError: true,
+            revalidate: false
         }).catch(() => {
-            toast.error("Failed to delete message")
+            toast.error("Failed to delete message.")
         })
     }
     return { deleteMessage, loading }
