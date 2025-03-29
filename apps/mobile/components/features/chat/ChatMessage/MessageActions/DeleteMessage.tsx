6b227fc3ba58eb660bd18fcb1198040ff9de1d71
Nikhil Kothari <nik.kothari22@live.com>
2025-03-29 14:49:27 +0530

    fix(mobile): alerts on android

--- apps/mobile/components/features/chat/ChatMessage/MessageActions/DeleteMessage.tsx
+++ apps/mobile/components/features/chat/ChatMessage/MessageActions/DeleteMessage.tsx
@@ -1,9 +1,7 @@
-import { Alert } from '@components/nativewindui/Alert'
-import { AlertRef } from '@components/nativewindui/Alert/types'
 import { Message } from '@raven/types/common/Message'
 import { useFrappeDeleteDoc, useSWRConfig } from 'frappe-react-sdk'
-import React, { useCallback } from 'react'
-import { Pressable, View } from 'react-native'
+import { useCallback } from 'react'
+import { Alert, Pressable, View } from 'react-native'
 import { toast } from 'sonner-native'
 import { Text } from '@components/nativewindui/Text'
 import TrashIcon from "@assets/icons/TrashIcon.svg"
@@ -17,13 +15,20 @@ interface DeleteMessageProps {
 
 const DeleteMessage = ({ message, onClose }: DeleteMessageProps) => {
 
-    const deleteAlertRef = React.useRef<AlertRef>(null)
+    // const deleteAlertRef = React.useRef<AlertRef>(null)
     const { deleteMessage, loading } = useMessageDelete(message)
 
+    const onDelete = () => {
+        deleteMessage()
+            .then(() => onClose())
+    }
+
     const onMessageDelete = useCallback(() => {
-        onClose()
-        deleteAlertRef.current?.show()
-    }, [deleteAlertRef, onClose])
+        Alert.alert('Delete message?', 'Are you sure you want to delete this message? It will be deleted for all users.', [
+            { text: 'Cancel', style: 'cancel' },
+            { text: 'Delete', style: 'destructive', onPress: onDelete },
+        ])
+    }, [])
 
     const { isDarkColorScheme } = useColorScheme()
 
@@ -31,30 +36,13 @@ const DeleteMessage = ({ message, onClose }: DeleteMessageProps) => {
         <View>
             <Pressable
                 onPress={onMessageDelete}
+                hitSlop={10}
                 className='flex flex-row items-center gap-3 p-2 rounded-lg ios:active:bg-destructive/5 dark:ios:active:bg-destructive/10'
                 android_ripple={{ color: 'rgba(0,0,0,0.1)', borderless: false }}
                 disabled={loading}>
                 <TrashIcon width={18} height={18} fill={isDarkColorScheme ? '#f87171' : '#dc2626'} />
                 <Text className='text-base text-red-600 dark:text-red-400'>Delete message</Text>
             </Pressable>
-            <Alert
-                key="delete-message"
-                ref={deleteAlertRef}
-                title="Delete message?"
-                message="Are you sure you want to delete this message? It will be deleted for all users."
-                buttons={[
-                    {
-                        text: 'Cancel',
-                    },
-                    {
-                        text: 'Delete',
-                        style: "destructive",
-                        onPress: () => {
-                            deleteMessage(onClose)
-                        }
-                    },
-                ]}
-            />
         </View>
     )
 }
@@ -67,7 +55,7 @@ const useMessageDelete = (message: Message) => {
 
     const { deleteDoc, loading } = useFrappeDeleteDoc()
 
-    const deleteMessage = (onSuccess: () => void) => {
+    const deleteMessage = async () => {
 
         const removeMessageFromCache = (data?: GetMessagesResponse): GetMessagesResponse => {
 
@@ -85,13 +73,12 @@ const useMessageDelete = (message: Message) => {
         }
 
         // Delete the message optimistically
-        mutate({ path: `get_messages_for_channel_${message.channel_id}` }, async (data?: GetMessagesResponse) => {
+        return mutate({ path: `get_messages_for_channel_${message.channel_id}` }, async (data?: GetMessagesResponse) => {
             // Make the request
             return deleteDoc('Raven Message', message.name).then(() => {
                 toast.success('Message deleted', {
                     duration: 500,
                 })
-                onSuccess()
             }).then(() => {
                 return removeMessageFromCache(data)
             })
