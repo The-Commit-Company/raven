ea2039e2fa6a73036d2244193ecd54e8348c089f
Nikhil Kothari <nik.kothari22@live.com>
2025-04-01 23:03:01 +0530

    chore(deps): update all Tiptap packages to latest

--- frontend/src/utils/dateConversions/utils.ts
+++ frontend/src/utils/dateConversions/utils.ts
@@ -1,4 +1,4 @@
-import * as dayjs from 'dayjs'
+import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
