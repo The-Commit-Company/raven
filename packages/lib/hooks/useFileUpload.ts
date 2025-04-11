12ad695f394fda098fbfb6fc1bba89e323fc3ed1
Nikhil Kothari <nik.kothari22@live.com>
2025-04-11 16:44:47 +0530

    fix(mobile): update chat stream on new message sent (#1604)

--- packages/lib/hooks/useFileUpload.ts
+++ packages/lib/hooks/useFileUpload.ts
@@ -1,8 +1,9 @@
-import { SetStateAction, useContext, useState } from 'react'
-import { FrappeConfig, FrappeContext } from 'frappe-react-sdk'
-import { CustomFile } from '@raven/types/common/File'
+import { useContext } from 'react'
+import { FrappeConfig, FrappeContext, useSWRConfig } from 'frappe-react-sdk'
 import { filesAtomFamily } from '@lib/ChatInputUtils'
 import { useAtom } from 'jotai'
+import { RavenMessage } from '@raven/types/RavenMessaging/RavenMessage'
+import { GetMessagesResponse } from '@raven/types/common/ChatStream'
 export interface FileUploadProgress {
   progress: number,
   isComplete: boolean,
@@ -10,8 +11,61 @@ export interface FileUploadProgress {
 
 export default function useFileUpload(siteID: string, channelID: string) {
 
+  const { mutate } = useSWRConfig()
+
   const { file } = useContext(FrappeContext) as FrappeConfig
   const [files, setFiles] = useAtom(filesAtomFamily(siteID + channelID))
+
+  const onMessageSendCompleted = (messages: RavenMessage[]) => {
+    // Update the messages in the cache
+
+    mutate({ path: `get_messages_for_channel_${channelID}` }, (data?: GetMessagesResponse) => {
+      if (data && data?.message.has_new_messages) {
+        return data
+      }
+
+      const existingMessages = data?.message.messages ?? []
+
+      const newMessages = [...existingMessages]
+
+      messages.forEach(message => {
+        // Check if the message is already present in the messages array
+        const messageIndex = existingMessages.findIndex(m => m.name === message.name)
+
+        if (messageIndex !== -1) {
+          // If the message is already present, update the message
+          // @ts-ignore
+          newMessages[messageIndex] = {
+            ...message,
+            _liked_by: "",
+            is_pinned: 0,
+            is_continuation: 0
+          }
+        } else {
+          // If the message is not present, add the message to the array
+          // @ts-ignore
+          newMessages.push({
+            ...message,
+            _liked_by: "",
+            is_pinned: 0,
+            is_continuation: 0
+          })
+        }
+      })
+
+      return {
+        message: {
+          messages: newMessages.sort((a, b) => {
+            return new Date(b.creation).getTime() - new Date(a.creation).getTime()
+          }),
+          has_new_messages: false,
+          has_old_messages: data?.message.has_old_messages ?? false
+        }
+      }
+
+    }, { revalidate: false })
+
+  }
 
   const uploadFiles = async () => {
 
@@ -40,7 +94,8 @@ export default function useFileUpload(siteID: string, channelID: string) {
             })
           },
           'raven.api.upload_file.upload_file_with_message'
-        ).then(() => {
+        ).then((res) => {
+          onMessageSendCompleted([res.data.message])
           setFiles((prevFiles) => {
             return prevFiles.map((file) => {
               if (file.fileID === f.fileID) {
