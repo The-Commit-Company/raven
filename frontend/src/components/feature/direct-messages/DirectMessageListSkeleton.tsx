0a80a093b1b81fff9b55fe2295b98138cca1a844
hungtech511 <vhung.webdev@gmail.com>
2025-05-25 14:36:03 +0700

    add scroll sidebar horizontally

---
+++ frontend/src/components/feature/direct-messages/DirectMessageListSkeleton.tsx
@@ -0,0 +1,53 @@
+import { Flex, Text } from '@radix-ui/themes'
+import { SidebarGroup, SidebarGroupItem, SidebarGroupLabel, SidebarGroupList, SidebarIcon, SidebarItem } from '../../layout/Sidebar/SidebarComp'
+import classNames from 'classnames'
+
+const SKELETON_ITEM_COUNT = 5
+
+export const DirectMessageListSkeleton = () => {
+  return (
+    <div>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+    <p>is Loading</p>
+
+    </div>
+)
+}
