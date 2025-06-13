fe4003709d953c0530755357aead78996a3f059a
MinhKhang0803 <khang080803@gmail.com>
2025-06-13 11:51:50 +0700

    sửa lỗi doctype

---
+++ raven/chatbot/doctype/chatconversation/chatconversation.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2025, The Commit Company (Algocode Technologies Pvt. Ltd.) and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ChatConversation(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+		from raven.chatbot.doctype.chatmessage.chatmessage import ChatMessage
+
+		created_on: DF.Datetime | None
+		messages: DF.Table[ChatMessage]
+		title: DF.Data | None
+		user: DF.Link | None
+	# end: auto-generated types
+	pass
---
+++ raven/chatbot/doctype/chatconversation/test_chatconversation.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, The Commit Company (Algocode Technologies Pvt. Ltd.) and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestChatConversation(FrappeTestCase):
+	pass
