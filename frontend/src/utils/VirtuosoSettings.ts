0f5a936c82c3a476c9c7c66bd7ab757d7f6ff7fd
phamducbinhh <phamducbinh1712000@gmail.com>
2025-06-15 16:06:42 +0700

    Refactor Scroll To Virtouso

--- frontend/src/utils/VirtuosoSettings.ts
+++ frontend/src/utils/VirtuosoSettings.ts
@@ -9,20 +9,20 @@ export const virtuosoSettings = {
   increaseViewportBy: performanceLevel === 'high' ? 800 : performanceLevel === 'mid' ? 500 : 300,
   overscan: performanceLevel === 'high' ? 80 : performanceLevel === 'mid' ? 50 : 30,
   initialItemCount: performanceLevel === 'high' ? 50 : performanceLevel === 'mid' ? 30 : 15,
-  defaultItemHeight: 60,
-  scrollSeekConfiguration: {
-    enter: (v: number) =>
-      performanceLevel === 'high'
-        ? Math.abs(v) > 2000
-        : performanceLevel === 'mid'
-          ? Math.abs(v) > 1000
-          : Math.abs(v) > 600,
-    exit: (v: number) =>
-      performanceLevel === 'high'
-        ? Math.abs(v) < 100
-        : performanceLevel === 'mid'
-          ? Math.abs(v) < 50
-          : Math.abs(v) < 30,
-    change: () => null
-  }
+  defaultItemHeight: 60
+  // scrollSeekConfiguration: {
+  //   enter: (v: number) =>
+  //     performanceLevel === 'high'
+  //       ? Math.abs(v) > 2000
+  //       : performanceLevel === 'mid'
+  //         ? Math.abs(v) > 1000
+  //         : Math.abs(v) > 600,
+  //   exit: (v: number) =>
+  //     performanceLevel === 'high'
+  //       ? Math.abs(v) < 100
+  //       : performanceLevel === 'mid'
+  //         ? Math.abs(v) < 50
+  //         : Math.abs(v) < 30,
+  //   change: () => null
+  // }
 }
