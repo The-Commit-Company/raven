d01b841f13595d754ba2b0155a58e9e1db8bad27
Aditya Patil <patiladitya781@gmail.com>
2025-09-17 14:30:06 +0530

    feat: extract all links from message

--- raven/raven_messaging/doctype/raven_message/raven_message.py
+++ raven/raven_messaging/doctype/raven_message/raven_message.py
@@ -27,15 +27,12 @@ from raven.utils import (
 
 class RavenMessage(Document):
 	# begin: auto-generated types
-	# ruff: noqa
-
 	# This code is auto-generated. Do not modify anything in this block.
 
 	from typing import TYPE_CHECKING
 
 	if TYPE_CHECKING:
 		from frappe.types import DF
-
 		from raven.raven_messaging.doctype.raven_mention.raven_mention import RavenMention
 
 		blurhash: DF.SmallText | None
@@ -56,6 +53,7 @@ class RavenMessage(Document):
 		link_doctype: DF.Link | None
 		link_document: DF.DynamicLink | None
 		linked_message: DF.Link | None
+		links: DF.SmallText | None
 		mentions: DF.Table[RavenMention]
 		message_reactions: DF.JSON | None
 		message_type: DF.Literal["Text", "Image", "File", "Poll", "System"]
@@ -65,7 +63,6 @@ class RavenMessage(Document):
 		text: DF.LongText | None
 		thumbnail_height: DF.Data | None
 		thumbnail_width: DF.Data | None
-	# ruff: noqa
 	# end: auto-generated types
 
 	def before_validate(self):
@@ -84,7 +81,7 @@ class RavenMessage(Document):
 		1. Extract all user mentions
 		2. Remove empty trailing paragraphs
 		3. Extract the text content
-		4. TODO: Extract all links
+		4. Extract all links and store as | separated string
 		"""
 		if not self.text:
 			return
@@ -94,6 +91,14 @@ class RavenMessage(Document):
 		soup = BeautifulSoup(self.text, "html.parser")
 		self.remove_empty_trailing_paragraphs(soup)
 		self.extract_mentions(soup)
+
+		links = []
+		for link in soup.find_all("a"):
+			href = link.get("href")
+			if href:
+				links.append(href)
+		if links:
+			self.links = "|" + "|".join(links) + "|"
 
 		text_content = soup.get_text(" ", strip=True)
 
