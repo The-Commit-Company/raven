028768497d7e8ad80454f579975b3ba85fc11be1
barredterra <14891507+barredterra@users.noreply.github.com>
2026-03-08 20:59:21 +0100

    feat: allow functions to access any doctype
    Resolves #2055

--- raven/raven_bot/doctype/raven_bot/test_raven_bot.py
+++ raven/raven_bot/doctype/raven_bot/test_raven_bot.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2024, The Commit Company and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestRavenBot(FrappeTestCase):
-	pass
+	def test_bot_allows_any_doctype_functions_without_extra_flag(self):
+		function = frappe.get_doc(
+			{
+				"doctype": "Raven AI Function",
+				"function_name": f"test_get_doc_{frappe.generate_hash(length=6)}",
+				"description": "Fetch any permitted document",
+				"type": "Get Document",
+				"allow_any_doctype": 1,
+			}
+		).insert()
+
+		bot = frappe.new_doc("Raven Bot")
+		bot.bot_name = f"Test Bot {frappe.generate_hash(length=6)}"
+		bot.bot_functions = [frappe._dict(function=function.name)]
+		bot.validate_functions()
