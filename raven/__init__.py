4b1752e748ee03aa819c1ec9d7bc4bed494c4f6a
Nikhil Kothari <nik.kothari22@live.com>
2026-03-13 02:30:41 +0530

    feat: allow viewing original message in channel

--- raven/__init__.py
+++ raven/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.8.6"
+__version__ = "2.8.7"
 
 from raven.raven_integrations.doctype.raven_incoming_webhook.raven_incoming_webhook import (  # noqa
 	handle_incoming_webhook as webhook,
