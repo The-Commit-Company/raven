62f918e05c47872cb11abae7b1101f65fe1dfbfb
prathameshkurunkar7 <prathamesh.kurunkar@thecommit.company>
2026-03-30 20:31:34 +0530

    refactor(notification): replace FrappeClient with make_api_call for API requests in notification handling

--- raven/scheduler/daily.py
+++ raven/scheduler/daily.py
@@ -8,33 +8,35 @@ from raven.raven_cloud_notifications import get_site_name
 def sync_invalid_tokens():
 	"""
 	This function is used to fetch the invalid tokens from Raven Cloud and sync them to the database.
-	It runs a loop to fetch the Invalid tokens in batches of 10 from Raven Cloud and deletes them from the database.
+	It runs a loop to fetch the Invalid tokens in batches of 100 from Raven Cloud and deletes them from the database.
 	If has_more is true, it will continue to fetch the next batch of tokens.
 	If has_more is false, it will break the loop. If there are no invalid tokens or error in fetching the invalid tokens, it will break the loop.
 	"""
+	from raven.utils import make_api_call
+
 	raven_settings = frappe.get_single("Raven Settings")
 
-	client = FrappeClient(
-		url=raven_settings.push_notification_server_url,
-		api_key=raven_settings.push_notification_api_key,
-		api_secret=raven_settings.get_password("push_notification_api_secret"),
-	)
+	url = f"{raven_settings.push_notification_server_url}/api/method/raven_cloud.api.notification.sync_invalid_tokens"
 
-	batch_size = 10
+	batch_size = 100
 
 	site_name = get_site_name()
 
 	while True:
 		try:
-			response = client.post_api(
-				"raven_cloud.api.notification.sync_invalid_tokens",
+			response = make_api_call(
+				url=url,
+				api_key=raven_settings.push_notification_api_key,
+				api_secret=raven_settings.get_password("push_notification_api_secret"),
+				method="POST",
 				params={
 					"site_name": site_name,
 					"batch_size": batch_size,
 				},
 			)
+			message = response.get("message")
 
-			invalid_tokens = response.get("invalid_tokens", [])
+			invalid_tokens = message.get("invalid_tokens", [])
 
 			if not invalid_tokens:
 				break
@@ -48,7 +50,7 @@ def sync_invalid_tokens():
 					)
 
 			# Check if more tokens exist
-			has_more = response.get("has_more", False)
+			has_more = message.get("has_more", False)
 			if not has_more:
 				break
 
