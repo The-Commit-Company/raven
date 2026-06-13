0d07e7886bfd23a4ae3f3101c67359d60ae89c4e
Nikhil Kothari <nik.kothari22@live.com>
2026-06-14 00:19:17 +0530

    fix: use Badge for date separator

--- apps/web/src/components/features/message/renderers/DateSeparator.tsx
+++ apps/web/src/components/features/message/renderers/DateSeparator.tsx
@@ -1,10 +1,11 @@
+import { Badge } from "@components/ui/badge";
 import { Separator } from "@components/ui/separator";
 
 export default function DateSeparator({ label }: { label: string }) {
     return (
         <div className="sticky top-0 z-20 py-2 -mx-8 px-8 flex items-center bg-surface-white">
             <Separator className="flex-1" />
-            <span className="mx-5 text-xs px-2 py-1 rounded-md text-ink-gray-4 font-normal bg-surface-gray-3/60">{label}</span>
+            <Badge variant="subtle" theme="gray" size='md'>{label}</Badge>
             <Separator className="flex-1" />
         </div>
     )
